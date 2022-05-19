434e51e49d13dcc5164232f624cfd77c27d9debc
洛水.山岭居室 <luoshuijs@foxmail.com>
2022-05-19 19:04:48 +0800

    添加退出指定群组命令

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,6 +1,8 @@
 from telegram import Update
+from telegram.error import BadRequest
 from telegram.ext import CallbackContext
 
+from logger import Log
 from plugins.base import BasePlugins
 from service import BaseService
 
@@ -40,5 +42,36 @@ class Admin(BasePlugins):
                     await message.reply_text("删除成功")
                 else:
                     await message.reply_text("该用户不存在管理员列表")
+        else:
+            await message.reply_text("权限不足")
+
+    async def leave_chat(self, update: Update, context: CallbackContext):
+        message = update.message
+        user = message.from_user
+        admin_list = await self.service.admin.get_admin_list()
+        if user.id in admin_list:
+            try:
+                args = message.text.split()
+                if len(args) >= 2:
+                    char_id = int(args[1])
+                else:
+                    await message.reply_text("输入错误")
+                    return
+            except ValueError as error:
+                Log.error(f"获取 char_id 发生错误！ 错误信息为 \n", error)
+                await message.reply_text("输入错误")
+                return
+            try:
+                try:
+                    char = await context.bot.get_chat(char_id)
+                    await message.reply_text(f"正在尝试退出群 {char.title}[{char.id}]")
+                except BadRequest:
+                    pass
+                await context.bot.leave_chat(char_id)
+            except BadRequest as error:
+                Log.error(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 \n", error)
+                await message.reply_text(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 {str(error)}")
+                return
+            await message.reply_text(f"退出 char_id[{char_id}] 成功！")
         else:
             await message.reply_text("权限不足")
