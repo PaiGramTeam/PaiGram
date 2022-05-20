387183188191553e2488c94e3e09b4cbf5e68abe
xtaodada <xtao@xtaolink.cn>
2022-05-20 16:34:03 +0800

    Admin 类使用装饰器精简代码

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from telegram import Update
 from telegram.error import BadRequest, Forbidden
 from telegram.ext import CallbackContext
@@ -11,67 +13,67 @@ class Admin(BasePlugins):
     def __init__(self, service: BaseService):
         super().__init__(service)
 
+    def bot_admins_only(func: Callable) -> Callable:  # noqa
+        async def decorator(self, update: Update, context: CallbackContext):
+            admin_list = await self.service.admin.get_admin_list()
+            if update.message.from_user.id in admin_list:
+                return await func(self, update, context)
+            else:
+                await update.message.reply_text("权限不足")
+
+        return decorator
+
+    @bot_admins_only  # noqa
     async def add_admin(self, update: Update, context: CallbackContext):
         message = update.message
-        user = message.from_user
         reply_to_message = message.reply_to_message
         admin_list = await self.service.admin.get_admin_list()
-        if user.id in admin_list:
-            if reply_to_message is None:
-                await message.reply_text("请回复对应消息")
-            else:
-                if reply_to_message.from_user.id in admin_list:
-                    await message.reply_text("该用户已经存在管理员列表")
-                else:
-                    await self.service.admin.add_admin(reply_to_message.from_user.id)
-                    await message.reply_text("添加成功")
+        if reply_to_message is None:
+            await message.reply_text("请回复对应消息")
         else:
-            await message.reply_text("权限不足")
+            if reply_to_message.from_user.id in admin_list:
+                await message.reply_text("该用户已经存在管理员列表")
+            else:
+                await self.service.admin.add_admin(reply_to_message.from_user.id)
+                await message.reply_text("添加成功")
 
+    @bot_admins_only  # noqa
     async def del_admin(self, update: Update, context: CallbackContext):
         message = update.message
-        user = message.from_user
         reply_to_message = message.reply_to_message
         admin_list = await self.service.admin.get_admin_list()
-        if user.id in admin_list:
-            if reply_to_message is None:
-                await message.reply_text("请回复对应消息")
-            else:
-                if reply_to_message.from_user.id in admin_list:
-                    await self.service.admin.delete_admin(reply_to_message.from_user.id)
-                    await message.reply_text("删除成功")
-                else:
-                    await message.reply_text("该用户不存在管理员列表")
+        if reply_to_message is None:
+            await message.reply_text("请回复对应消息")
         else:
-            await message.reply_text("权限不足")
+            if reply_to_message.from_user.id in admin_list:
+                await self.service.admin.delete_admin(reply_to_message.from_user.id)
+                await message.reply_text("删除成功")
+            else:
+                await message.reply_text("该用户不存在管理员列表")
 
+    @bot_admins_only  # noqa
     async def leave_chat(self, update: Update, context: CallbackContext):
         message = update.message
-        user = message.from_user
-        admin_list = await self.service.admin.get_admin_list()
-        if user.id in admin_list:
-            try:
-                args = message.text.split()
-                if len(args) >= 2:
-                    char_id = int(args[1])
-                else:
-                    await message.reply_text("输入错误")
-                    return
-            except ValueError as error:
-                Log.error(f"获取 char_id 发生错误！ 错误信息为 \n", error)
+        try:
+            args = message.text.split()
+            if len(args) >= 2:
+                char_id = int(args[1])
+            else:
                 await message.reply_text("输入错误")
                 return
+        except ValueError as error:
+            Log.error(f"获取 char_id 发生错误！ 错误信息为 \n", error)
+            await message.reply_text("输入错误")
+            return
+        try:
             try:
-                try:
-                    char = await context.bot.get_chat(char_id)
-                    await message.reply_text(f"正在尝试退出群 {char.title}[{char.id}]")
-                except (BadRequest, Forbidden):
-                    pass
-                await context.bot.leave_chat(char_id)
-            except (BadRequest, Forbidden) as error:
-                Log.error(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 \n", error)
-                await message.reply_text(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 {str(error)}")
-                return
-            await message.reply_text(f"退出 char_id[{char_id}] 成功！")
-        else:
-            await message.reply_text("权限不足")
+                char = await context.bot.get_chat(char_id)
+                await message.reply_text(f"正在尝试退出群 {char.title}[{char.id}]")
+            except (BadRequest, Forbidden):
+                pass
+            await context.bot.leave_chat(char_id)
+        except (BadRequest, Forbidden) as error:
+            Log.error(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 \n", error)
+            await message.reply_text(f"退出 char_id[{char_id}] 发生错误！ 错误信息为 {str(error)}")
+            return
+        await message.reply_text(f"退出 char_id[{char_id}] 成功！")
