1f65f41224df042837a80c2f481aa6ec738303bc
洛水.山岭居室 <luoshuijs@foxmail.com>
2022-05-25 18:35:13 +0800

    修改 `plugins.sign` 获取国际服Cookie的错误 删除无用代码

--- plugins/sign.py
+++ plugins/sign.py
@@ -7,7 +7,6 @@ from telegram.ext import CallbackContext, ConversationHandler, filters
 
 from logger import Log
 from model.base import ServiceEnum
-from model.genshinhelper import YuanShen, Genshin
 from plugins.base import BasePlugins
 from service import BaseService
 from service.base import UserInfoData
@@ -22,18 +21,16 @@ class SignCommandData:
 class Sign(BasePlugins):
     def __init__(self, service: BaseService):
         super().__init__(service)
-        self._sign_yuanshen = YuanShen()
-        self._sing_genshin = Genshin()
 
     CHECK_SERVER, COMMAND_RESULT = range(10400, 10402)
 
     @staticmethod
-    async def _start_sign_ex(user_info: UserInfoData, service: ServiceEnum) -> str:
+    async def _start_sign(user_info: UserInfoData, service: ServiceEnum) -> str:
         if service == ServiceEnum.MIHOYOBBS:
             client = genshin.ChineseClient(cookies=user_info.mihoyo_cookie)
             uid = user_info.mihoyo_game_uid
         else:
-            client = genshin.GenshinClient(cookies=user_info.mihoyo_cookie, lang="zh-cn")
+            client = genshin.GenshinClient(cookies=user_info.hoyoverse_cookie, lang="zh-cn")
             uid = user_info.hoyoverse_game_uid
         try:
             rewards = await client.get_monthly_rewards(game=Game.GENSHIN, lang="zh-cn")
@@ -68,45 +65,6 @@ class Sign(BasePlugins):
                   f"签到结果: {result}"
         return message
 
-    async def _start_sign(self, user_info: UserInfoData, service: ServiceEnum) -> str:
-        if service == ServiceEnum.MIHOYOBBS:
-            sign_api = self._sign_yuanshen
-            uid = user_info.mihoyo_game_uid
-            cookies = user_info.mihoyo_cookie
-        else:
-            sign_api = self._sing_genshin
-            uid = user_info.hoyoverse_game_uid
-            cookies = user_info.hoyoverse_cookie
-        sign_give = await sign_api.get_sign_give(cookies=cookies)
-        if sign_give.error:
-            Log.error(f"UID {uid} 获取签到信息失败，API返回信息为 {sign_give.message}")
-            return f"获取签到信息失败，API返回信息为 {sign_give.message}"
-        is_sign = await sign_api.is_sign(uid, cookies=cookies)
-        if is_sign.error:
-            Log.error(f"UID {uid} 获取签到信息失败，API返回信息为 {is_sign.message}")
-            return f"获取签到状态失败，API返回信息为 {is_sign.message}"
-        total_sign_day = is_sign.data["total_sign_day"]
-        award_name = sign_give.data["awards"][total_sign_day - 1]["name"]
-        award_cnt = sign_give.data["awards"][total_sign_day - 1]["cnt"]
-        today = is_sign.data["today"]
-        if not is_sign.data["is_sign"]:
-            sign = await sign_api.sign(uid, cookies=cookies)
-            if sign.code == 0:
-                result = "OK"
-            elif sign.code == -5003:
-                result = "今天旅行者已经签到过了~"
-            else:
-                Log.error(f"UID {uid} 签到失败 返回 错误代码 {sign.code} 错误信息 {sign.message}")
-                result = f"签到失败 返回 错误代码 {sign.code} 错误信息 {sign.message}"
-        else:
-            result = "今天旅行者已经签到过了~"
-        Log.info(f"UID {uid} 签到结果 {result}")
-        message = f"###### {today} ######\n" \
-                  f"UID: {uid}\n" \
-                  f"今日奖励: {award_name} × {award_cnt}\n" \
-                  f"签到结果: {result}"
-        return message
-
     async def command_start(self, update: Update, context: CallbackContext) -> int:
         user = update.effective_user
         message = update.message
@@ -135,7 +93,7 @@ class Sign(BasePlugins):
             sign_command_data.reply_to_message_id = update.message.message_id
             return self.COMMAND_RESULT
         else:
-            sign = await self._start_sign_ex(user_info, user_info.service)
+            sign = await self._start_sign(user_info, user_info.service)
             reply_message = await message.reply_text(sign)
             if filters.ChatType.GROUPS.filter(reply_message):
                 self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
@@ -148,9 +106,9 @@ class Sign(BasePlugins):
         await query.answer()
         message = "签到失败"
         if query.data == "sign|米游社":
-            message = await self._start_sign_ex(user_info, ServiceEnum.MIHOYOBBS)
+            message = await self._start_sign(user_info, ServiceEnum.MIHOYOBBS)
         if query.data == "sign|HoYoLab":
-            message = await self._start_sign_ex(user_info, ServiceEnum.HOYOLAB)
+            message = await self._start_sign(user_info, ServiceEnum.HOYOLAB)
         await query.edit_message_text(message)
         if query_message := query.message:
             if filters.ChatType.GROUPS.filter(query_message):
