cb96056bcc612dd9b1688903fb665be42d8faefe
洛水.山岭居室 <luoshuijs@foxmail.com>
2022-05-28 20:53:54 +0800

    修改 `plugins.help` 在第二次调用时候使用 `file_id` 发送图片
    速度更快！

--- plugins/help.py
+++ plugins/help.py
@@ -11,11 +11,16 @@ class Help(BasePlugins):
     def __init__(self, service: BaseService):
         super().__init__(service)
         self.help_png = None
+        self.file_id = None
 
     async def command_start(self, update: Update, _: CallbackContext) -> None:
         message = update.message
         user = update.effective_user
         Log.info(f"用户 {user.full_name}[{user.id}] 帮助命令")
-        if self.help_png is None or config.DEBUG:
-            self.help_png = await self.service.template.render('bot', "help.html", {}, {"width": 768, "height": 768})
-        await message.reply_photo(self.help_png, filename=f"help.png", allow_sending_without_reply=True)
+        if self.file_id is None or config.DEBUG:
+            help_png = await self.service.template.render('bot', "help.html", {}, {"width": 768, "height": 768})
+            reply_photo = await message.reply_photo(help_png, filename=f"help.png", allow_sending_without_reply=True)
+            photo = reply_photo.photo[0]
+            self.file_id = photo.file_id
+        else:
+            await message.reply_photo(self.file_id, allow_sending_without_reply=True)
