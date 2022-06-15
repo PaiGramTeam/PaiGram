20f45ad5be04c8d6e89eac96ad2785075e457f37
洛水居室 <luoshuijs@outlook.com>
2022-06-15 19:53:47 +0800

    删除 `weapons.json` 数据

--- metadata/metadata.py
+++ metadata/metadata.py
@@ -9,13 +9,9 @@ class Metadata:
         characters_file = os.path.join(project_path, 'characters.json')
         with open(characters_file, 'r', encoding='utf-8') as f:
             self.characters = ujson.load(f)
-        weapons_file = os.path.join(project_path, 'weapons.json')
-        with open(weapons_file, 'r', encoding='utf-8') as f:
-            self.weapons = ujson.load(f)
 
         # 初始化
         self.characters_name_list: List[str] = [characters["Name"] for characters in self.characters]
-        self.weapons_name_list: List[str] = [weapons["Name"] for weapons in self.weapons]
 
     @staticmethod
     def get_info(data: dict, name: str) -> dict:
