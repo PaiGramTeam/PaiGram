42fe9bc85a7881d8805557e72ad1377bbd9efe23
洛水居室 <luoshuijs@outlook.com>
2022-06-18 00:16:36 +0800

    移动文件 重构代码

--- ci.py
+++
@@ -1,7 +0,0 @@
-from httpx import AsyncClient, get
-""" Init httpx client """
-# 使用自定义 UA
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-}
-client = AsyncClient(timeout=10.0, headers=headers)
