b89c00d88e3c95eb9c139fb727ecf63ac88d4cfa
洛水居室 <luoshuijs@outlook.com>
2022-06-22 19:40:59 +0800

    尝试修复

--- plugins/start.py
+++ plugins/start.py
@@ -1,11 +1,11 @@
 from telegram import Update, ReplyKeyboardRemove
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, filters
 from telegram.helpers import escape_markdown
 
 from plugins.base import restricts
 
 
-@restricts
+@restricts()
 async def start(update: Update, _: CallbackContext) -> None:
     user = update.effective_user
     message = update.message
@@ -17,26 +17,26 @@ async def start(update: Update, _: CallbackContext) -> None:
     await update.message.reply_markdown_v2(f"你好 {user.mention_markdown_v2()} {escape_markdown('！我是派蒙 ！')}")
 
 
-@restricts
+@restricts()
 async def help_command(update: Update, _: CallbackContext) -> None:
     await update.message.reply_text("前面的区域，以后再来探索吧！")
 
 
-@restricts
+@restricts()
 async def unknown_command(update: Update, _: CallbackContext) -> None:
     await update.message.reply_text("前面的区域，以后再来探索吧！")
 
 
-@restricts
+@restricts()
 async def emergency_food(update: Update, _: CallbackContext) -> None:
     await update.message.reply_text("派蒙才不是应急食品！")
 
 
-@restricts
+@restricts(filters.ALL)
 async def ping(update: Update, _: CallbackContext) -> None:
     await update.message.reply_text("online! ヾ(✿ﾟ▽ﾟ)ノ")
 
 
-@restricts
+@restricts(filters.ALL)
 async def reply_keyboard_remove(update: Update, _: CallbackContext) -> None:
     await update.message.reply_text("移除远程键盘成功", reply_markup=ReplyKeyboardRemove())
