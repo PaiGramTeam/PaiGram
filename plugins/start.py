774d8cefa07afc8127a5e7ffc982b30eb78d8088
洛水居室 <luoshuijs@outlook.com>
2022-06-24 19:46:07 +0800

    更新 `start` 命令

--- plugins/start.py
+++ plugins/start.py
@@ -1,19 +1,26 @@
+from pyrogram.methods.messages.send_chat_action import ChatAction
 from telegram import Update, ReplyKeyboardRemove
+from telegram.error import BadRequest
 from telegram.ext import CallbackContext
 from telegram.helpers import escape_markdown
 
+from config import config
+from logger import Log
+from utils.base import PaimonContext
 from plugins.base import restricts
 
 
 @restricts()
-async def start(update: Update, _: CallbackContext) -> None:
+async def start(update: Update, context: PaimonContext) -> None:
     user = update.effective_user
     message = update.message
-    args = message.text.split(" ")
-    if len(args) >= 2 and args[1] == "inline_message":
-        await update.message.reply_markdown_v2(f"你好 {user.mention_markdown_v2()} {escape_markdown('！我是派蒙 ！')}\n"
-                                               f"{escape_markdown('发送 /help 命令即可查看命令帮助')}")
-        return
+    args = context.args
+    if args is not None:
+        if len(args) >= 1:
+            if args[1] == "inline_message":
+                await message.reply_markdown_v2(f"你好 {user.mention_markdown_v2()} {escape_markdown('！我是派蒙 ！')}\n"
+                                                f"{escape_markdown('发送 /help 命令即可查看命令帮助')}")
+                return
     await update.message.reply_markdown_v2(f"你好 {user.mention_markdown_v2()} {escape_markdown('！我是派蒙 ！')}")
 
 
