6433ddba87e58e174e0d18c3a233b3b7e3739e6d
洛水居室 <luoshuijs@outlook.com>
2022-06-28 12:42:17 +0800

    添加新的数据结构

--- model/game/artifact.py
+++ model/game/artifact.py
@@ -13,14 +13,24 @@ class ArtifactInfo(BaseObject):
 
     def __init__(self, item_id: int = 0, name: str = "", level: int = 0, main_item: Optional[GameItem] = None,
                  pos: Union[Enum, str] = "", star: int = 1, sub_item: Optional[List[GameItem]] = None, icon: str = ""):
+        """
+        :param item_id: item_id
+        :param name: 圣遗物名字
+        :param level: 圣遗物等级
+        :param main_item: 主词条
+        :param pos: 圣遗物类型
+        :param star: 星级
+        :param sub_item: 副词条
+        :param icon: 图片
+        """
         self.icon = icon
         self.item_id = item_id
-        self.level = level  # 等级
-        self.main_item = main_item  # 主属性
-        self.name = name  # 名称
-        self.pos = pos  # 圣遗物类型
-        self.star = star  # 星级
-        self.sub_item: List[GameItem] = []  # 词条列表
+        self.level = level
+        self.main_item = main_item
+        self.name = name
+        self.pos = pos
+        self.star = star
+        self.sub_item: List[GameItem] = []
         if sub_item is not None:
             self.sub_item = sub_item
 
