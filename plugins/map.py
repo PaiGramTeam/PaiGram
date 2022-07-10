dc996438eee956ec7348511cb1e6fc5c58f8557e
洛水居室 <luoshuijs@outlook.com>
2022-07-11 00:30:49 +0800

    ✨更新代码 未测试

--- plugins/map.py
+++ plugins/map.py
@@ -9,23 +9,17 @@ from logger import Log
 from manager import listener_plugins_class
 from plugins.base import BasePlugins, restricts
 from plugins.errorhandler import conversation_error_handler
-from service.map import get_resource_map_mes, get_resource_list_mes, init_point_list_and_map
+from service.map import MapHelper
 from utils.base import PaimonContext
-
-init_resource_map = False
-
-
-async def inquire_resource_list(message):
-    global init_resource_map
-    if not init_resource_map:
-        await init_point_list_and_map()
-        init_resource_map = True
-    text = get_resource_list_mes()
-    await message.reply_text(text)
 
 
 @listener_plugins_class()
 class Map(BasePlugins):
+
+    def __init__(self):
+        self.init_resource_map = False
+        self.map = MapHelper()
+
     @classmethod
     def create_handlers(cls) -> list:
         map_res = cls()
@@ -34,29 +28,36 @@ class Map(BasePlugins):
             MessageHandler(filters.Regex(r"^资源点查询(.*)"), map_res.command_start, block=True)
         ]
 
+    async def init_point_list_and_map(self):
+        Log.info("正在初始化地图资源节点")
+        if not self.init_resource_map:
+            await self.map.init_point_list_and_map()
+            self.init_resource_map = True
+
     @conversation_error_handler
     @restricts(restricts_time=20)
     async def command_start(self, update: Update, context: PaimonContext):
         message = update.message
         args = context.args
         user = update.effective_user
-        global init_resource_map
-        if not init_resource_map:
-            await init_point_list_and_map()
-            init_resource_map = True
+        await self.init_point_list_and_map()
         await message.reply_chat_action(ChatAction.TYPING)
         if len(args) >= 1:
             resource_name = args[0]
         else:
             Log.info(f"用户: {user.full_name} [{user.id}] 使用了 map 命令")
-            return await message.reply_text("请输入要查找的资源，或发送 `/map list` 查看资源列表", parse_mode="Markdown")
+            await message.reply_text("请输入要查找的资源，或发送 `/map list` 查看资源列表", parse_mode="Markdown")
+            return
         if resource_name in ("list", "列表"):
             Log.info(f"用户: {user.full_name} [{user.id}] 使用 map 命令查询了 资源列表")
-            return await inquire_resource_list(message)
+            text = await self.map.get_resource_list_mes()
+            await message.reply_text(text)
+            return
         Log.info(f"用户: {user.full_name} [{user.id}] 使用 map 命令查询了 {resource_name}")
-        text = await get_resource_map_mes(resource_name)
+        text = await self.map.get_resource_map_mes(resource_name)
         if "不知道" in text:
-            return await message.reply_text(text, parse_mode="Markdown")
+            await message.reply_text(text, parse_mode="Markdown")
+            return
         img = Image.open(f"cache{sep}map.jpg")
         if img.size[0] > 2048 or img.size[1] > 2048:
             await message.reply_document(open(f"cache{sep}map.jpg", mode='rb+'), caption=text)
--- service/map.py
+++ service/map.py
@@ -1,206 +1,248 @@
 import os
-import json
 import time
-from os.path import exists
-from PIL import Image, ImageMath
 from io import BytesIO
-from httpx import AsyncClient
+from typing import Optional, List
 
-LABEL_URL = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/map/label/tree?app_sn=ys_obc'
-POINT_LIST_URL = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/map/point/list?map_id=2&app_sn=ys_obc'
-MAP_URL = 'https://api-static.mihoyo.com/common/map_user/ys_obc/v1/map/info?map_id=2&app_sn=ys_obc&lang=zh-cn'
+import httpx
+import ujson
+from PIL import Image, ImageMath
 
-header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
-         "Chrome/84.0.4147.105 Safari/537.36"
+from model.helpers import REQUEST_HEADERS
 
-MAP_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, "resources", "icon", "map_icon.jpg")
 Image.MAX_IMAGE_PIXELS = None
 
-CENTER = None
-MAP_ICON = None
-
-zoom = 0.5
-resource_icon_offset = (-int(150 * 0.5 * zoom), -int(150 * zoom))
-
-data = {
-    "all_resource_type": {
-        # 这个字典保存所有资源类型
-        # "1": {
-        #         "id": 1,
-        #         "name": "传送点",
-        #         "icon": "",
-        #         "parent_id": 0,
-        #         "depth": 1,
-        #         "node_type": 1,
-        #         "jump_type": 0,
-        #         "jump_target_id": 0,
-        #         "display_priority": 0,
-        #         "children": []
-        #     },
-    },
-    "can_query_type_list": {
-        # 这个字典保存所有可以查询的资源类型名称和ID，这个字典只有名称和ID
-        # 上边字典里"depth": 2的类型才可以查询，"depth": 1的是1级目录，不能查询
-        # "七天神像":"2"
-        # "风神瞳":"5"
-
-    },
-    "all_resource_point_list": [
-        # 这个列表保存所有资源点的数据
-        # {
-        #     "id": 2740,
-        #     "label_id": 68,
-        #     "x_pos": -1789,
-        #     "y_pos": 2628,
-        #     "author_name": "✟紫灵心✟",
-        #     "ctime": "2020-10-29 10:41:21",
-        #     "display_state": 1
-        # },
-    ],
-    "date": ""  # 记录上次更新"all_resource_point_list"的日期
-}
-
-headers = {
-    'user-agent': header
-}
-client = AsyncClient(timeout=10.0, headers=headers)
+ZOOM = 0.5
+RESOURCE_ICON_OFFSET = (-int(150 * 0.5 * ZOOM), -int(150 * ZOOM))
 
 
-async def download_icon(url):
-    # 下载图片，返回Image对象
-    resp = await client.get(url=url)
-    if resp.status_code != 200:
-        raise ValueError(f"获取图片数据失败，错误代码 {resp.status_code}")
-    icon = resp.content
-    return Image.open(BytesIO(icon))
+class MapHelper:
+    LABEL_URL = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/map/label/tree?app_sn=ys_obc'
+    POINT_LIST_URL = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/map/point/list?map_id=2&app_sn=ys_obc'
+    MAP_URL = 'https://api-static.mihoyo.com/common/map_user/ys_obc/v1/map/info?map_id=2&app_sn=ys_obc&lang=zh-cn'
 
+    def __init__(self, cache_dir_name: str = "cache"):
+        self._current_dir = os.getcwd()
+        self._output_dir = os.path.join(self._current_dir, cache_dir_name)
+        self._resources_icon_dir = os.path.join(self._current_dir, "resources", "icon")
+        self._map_dir = os.path.join(self._resources_icon_dir, "map_icon.jpg")
+        self.client = httpx.AsyncClient(headers=REQUEST_HEADERS, timeout=10.0)
+        self.all_resource_type: dict = {}
+        """这个字典保存所有资源类型
+        
+        "1": {
+            "id": 1,
+            "name": "传送点",
+            "icon": "",
+            "parent_id": 0,
+            "depth": 1,
+            "node_type": 1,
+            "jump_type": 0,
+            "jump_target_id": 0,
+            "display_priority": 0,
+            "children": []
+        }
+        """
+        self.can_query_type_list: dict = {}
+        """这个字典保存所有可以查询的资源类型名称和ID，这个字典只有名称和ID
+        
+        上边字典里"depth": 2的类型才可以查询，"depth": 1的是1级目录，不能查询
+        
+        "七天神像":"2"
+        
+        "风神瞳":"5"
+        """
+        self.all_resource_point_list: list = []
+        """这个列表保存所有资源点的数据
+        
+        {
+            "id": 2740,
+            "label_id": 68,
+            "x_pos": -1789,
+            "y_pos": 2628,
+            "author_name": "作者名称",
+            "ctime": "更新时间",
+            "display_state": 1
+        }
+        """
+        self.date: str = ""
+        """记录上次更新"all_resource_point_list"的日期"""
 
-async def download_json(url):
-    # 获取资源数据，返回 JSON
-    if (
-            url == POINT_LIST_URL
-            and exists(f"""{os.path.dirname(__file__), os.path.pardir, "cache", os.sep}data{os.sep}list.json""")
-    ):
-        with open(f"assets{os.sep}data{os.sep}list.json", "rb") as f:
-            return json.loads(f.read())
-    resp = await client.get(url=url)
-    if resp.status_code != 200:
-        raise ValueError(f"获取资源点数据失败，错误代码 {resp.status_code}")
-    return resp.json()
+        self.center: Optional[List[float]] = None
+        """center"""
 
+        self.map_icon: Optional[Image] = None
+        """map_icon"""
 
-async def up_icon_image(sublist):
-    # 检查是否有图标，没有图标下载保存到本地
-    icon_id = sublist["id"]
-    icon_path = os.path.join(os.path.dirname(__file__), os.path.pardir, "resources", "icon", f"{icon_id}.png")
+    async def download_icon(self, url):
+        """下载图片 返回Image对象
+        :param url:
+        :return:
+        """
+        resp = await self.client.get(url=url)
+        if resp.status_code != 200:
+            raise ValueError(f"获取图片数据失败，错误代码 {resp.status_code}")
+        icon = resp.content
+        return Image.open(BytesIO(icon))
 
-    if not os.path.exists(icon_path):
-        icon_url = sublist["icon"]
-        icon = await download_icon(icon_url)
-        icon = icon.resize((150, 150))
+    async def download_json(self, url):
+        """
+        获取资源数据，返回 JSON
+        :param url:
+        :return: dict
+        """
+        resp = await self.client.get(url=url)
+        if resp.status_code != 200:
+            raise RuntimeError(f"获取资源点数据失败，错误代码 {resp.status_code}")
+        return resp.json()
 
-        box_alpha = Image.open(
-            os.path.join(os.path.dirname(__file__), os.path.pardir,
-                         "resources", "icon", "box_alpha.png")).getchannel("A")
-        box = Image.open(os.path.join(os.path.dirname(__file__), os.path.pardir, "resources", "icon", "box.png"))
+    async def init_point_list_and_map(self):
+        await self.up_label_and_point_list()
+        await self.up_map()
 
-        try:
-            icon_alpha = icon.getchannel("A")
-            icon_alpha = ImageMath.eval("convert(a*b/256, 'L')", a=icon_alpha, b=box_alpha)
-        except ValueError:
-            # 米游社的图有时候会没有alpha导致报错，这时候直接使用box_alpha当做alpha就行
-            icon_alpha = box_alpha
+    async def up_map(self):
+        """更新地图文件 并按照资源点的范围自动裁切掉不需要的地方
+        裁切地图需要最新的资源点位置，所以要先调用 up_label_and_point_list 再更新地图
+        :return: None
+        """
+        map_info = await self.download_json(self.MAP_URL)
+        map_info = map_info["data"]["info"]["detail"]
+        map_info = ujson.loads(map_info)
 
-        icon2 = Image.new("RGBA", (150, 150), "#00000000")
-        icon2.paste(icon, (0, -10))
+        map_url_list = map_info['slices'][0]
+        origin = map_info["origin"]
 
-        bg = Image.new("RGBA", (150, 150), "#00000000")
-        bg.paste(icon2, mask=icon_alpha)
-        bg.paste(box, mask=box)
+        x_start = map_info['total_size'][1]
+        y_start = map_info['total_size'][1]
+        x_end = 0
+        y_end = 0
+        for resource_point in self.all_resource_point_list:
+            x_pos = resource_point["x_pos"] + origin[0]
+            y_pos = resource_point["y_pos"] + origin[1]
+            x_start = min(x_start, x_pos)
+            y_start = min(y_start, y_pos)
+            x_end = max(x_end, x_pos)
+            y_end = max(y_end, y_pos)
 
-        with open(icon_path, "wb") as icon_file:
-            bg.save(icon_file)
+        x_start -= 200
+        y_start -= 200
+        x_end += 200
+        y_end += 200
 
+        self.center = [origin[0] - x_start, origin[1] - y_start]
+        x = int(x_end - x_start)
+        y = int(y_end - y_start)
+        self.map_icon = Image.new("RGB", (x, y))
+        x_offset = 0
+        for i in map_url_list:
+            map_url = i["url"]
+            map_icon = await self.download_icon(map_url)
+            self.map_icon.paste(map_icon, (int(-x_start) + x_offset, int(-y_start)))
+            x_offset += map_icon.size[0]
 
-async def up_label_and_point_list():
-    # 更新label列表和资源点列表
-    label_data = await download_json(LABEL_URL)
-    for label in label_data["data"]["tree"]:
-        data["all_resource_type"][str(label["id"])] = label
-        for sublist in label["children"]:
-            data["all_resource_type"][str(sublist["id"])] = sublist
-            data["can_query_type_list"][sublist["name"]] = str(sublist["id"])
-            await up_icon_image(sublist)
-        label["children"] = []
+    async def up_label_and_point_list(self):
+        """更新label列表和资源点列表
+        :return:
+        """
+        label_data = await self.download_json(self.LABEL_URL)
+        for label in label_data["data"]["tree"]:
+            self.all_resource_type[str(label["id"])] = label
+            for sublist in label["children"]:
+                self.all_resource_type[str(sublist["id"])] = sublist
+                self.can_query_type_list[sublist["name"]] = str(sublist["id"])
+                await self.up_icon_image(sublist)
+            label["children"] = []
+        test = await self.download_json(self.POINT_LIST_URL)
+        self.all_resource_type = test["data"]["point_list"]
+        self.date = time.strftime("%d")
 
-    test = await download_json(POINT_LIST_URL)
-    data["all_resource_point_list"] = test["data"]["point_list"]
-    data["date"] = time.strftime("%d")
+    async def up_icon_image(self, sublist: dict):
+        """检查是否有图标，没有图标下载保存到本地
+        :param sublist:
+        :return:
+        """
+        icon_id = sublist["id"]
+        icon_path = os.path.join(self._resources_icon_dir, f"{icon_id}.png")
 
+        if not os.path.exists(icon_path):
+            icon_url = sublist["icon"]
+            icon = await self.download_icon(icon_url)
+            icon = icon.resize((150, 150))
 
-async def up_map():
-    # 更新地图文件 并按照资源点的范围自动裁切掉不需要的地方
-    # 裁切地图需要最新的资源点位置，所以要先调用 up_label_and_point_list 再更新地图
-    global CENTER
-    global MAP_ICON
-    map_info = await download_json(MAP_URL)
-    map_info = map_info["data"]["info"]["detail"]
-    map_info = json.loads(map_info)
+            box_alpha = Image.open(
+                os.path.join(os.path.dirname(__file__), os.path.pardir,
+                             "resources", "icon", "box_alpha.png")).getchannel("A")
+            box = Image.open(os.path.join(os.path.dirname(__file__), os.path.pardir, "resources", "icon", "box.png"))
 
-    map_url_list = map_info['slices'][0]
-    origin = map_info["origin"]
+            try:
+                icon_alpha = icon.getchannel("A")
+                icon_alpha = ImageMath.eval("convert(a*b/256, 'L')", a=icon_alpha, b=box_alpha)
+            except ValueError:
+                # 米游社的图有时候会没有alpha导致报错，这时候直接使用box_alpha当做alpha就行
+                icon_alpha = box_alpha
 
-    x_start = map_info['total_size'][1]
-    y_start = map_info['total_size'][1]
-    x_end = 0
-    y_end = 0
-    for resource_point in data["all_resource_point_list"]:
-        x_pos = resource_point["x_pos"] + origin[0]
-        y_pos = resource_point["y_pos"] + origin[1]
-        x_start = min(x_start, x_pos)
-        y_start = min(y_start, y_pos)
-        x_end = max(x_end, x_pos)
-        y_end = max(y_end, y_pos)
+            icon2 = Image.new("RGBA", (150, 150), "#00000000")
+            icon2.paste(icon, (0, -10))
 
-    x_start -= 200
-    y_start -= 200
-    x_end += 200
-    y_end += 200
+            bg = Image.new("RGBA", (150, 150), "#00000000")
+            bg.paste(icon2, mask=icon_alpha)
+            bg.paste(box, mask=box)
 
-    CENTER = [origin[0] - x_start, origin[1] - y_start]
-    x = int(x_end - x_start)
-    y = int(y_end - y_start)
-    MAP_ICON = Image.new("RGB", (x, y))
-    x_offset = 0
-    for i in map_url_list:
-        map_url = i["url"]
-        map_icon = await download_icon(map_url)
-        MAP_ICON.paste(map_icon,
-                       (int(-x_start) + x_offset, int(-y_start)))
-        x_offset += map_icon.size[0]
+            with open(icon_path, "wb") as icon_file:
+                bg.save(icon_file)
 
+    async def get_resource_map_mes(self, name):
+        if self.date != time.strftime("%d"):
+            await self.init_point_list_and_map()
+        if name not in self.can_query_type_list:
+            return f"派蒙还不知道 {name} 在哪里呢，可以发送 `/map list` 查看资源列表"
+        resource_id = self.can_query_type_list[name]
+        map_res = ResourceMap(self.all_resource_point_list, self.map_icon, resource_id)
+        count = map_res.get_resource_count()
+        if not count:
+            return f"派蒙没有找到 {name} 的位置，可能米游社wiki还没更新"
+        map_res.gen_jpg()
+        mes = f"派蒙一共找到 {name} 的 {count} 个位置点\n* 数据来源于米游社wiki"
+        return mes
 
-async def init_point_list_and_map():
-    await up_label_and_point_list()
-    await up_map()
+    def get_resource_list_mes(self):
+        temp = {}
+        for list_id in self.all_resource_type:
+            # 先找1级目录
+            if self.all_resource_type[list_id]["depth"] == 1:
+                temp[list_id] = []
+        for list_id in self.all_resource_type:
+            # 再找2级目录
+            if self.all_resource_type[list_id]["depth"] == 2:
+                temp[str(self.all_resource_type[list_id]["parent_id"])].append(list_id)
+        mes = "当前资源列表如下：\n"
+
+        for resource_type_id in temp:
+            if resource_type_id in ["1", "12", "50", "51", "95", "131"]:
+                # 在游戏里能查到的数据这里就不列举了，不然消息太长了
+                continue
+            mes += f"{self.all_resource_type[resource_type_id]['name']}："
+            for resource_id in temp[resource_type_id]:
+                mes += f"{self.all_resource_type[resource_id]['name']}，"
+            mes += "\n"
+        return mes
 
 
 class ResourceMap:
 
-    def __init__(self, resource_name):
-        self.resource_id = str(data["can_query_type_list"][resource_name])
-
-        self.map_image = MAP_ICON.copy()  # noqa
+    def __init__(self, all_resource_point_list: List[dict], map_icon: Image, center: List[float], resource_id: int):
+        self.all_resource_point_list = all_resource_point_list
+        self.resource_id = resource_id
+        self.center = center
+        self.map_image = map_icon.copy()
         self.map_size = self.map_image.size
-
         # 地图要要裁切的左上角和右下角坐标
         # 这里初始化为地图的大小
         self.x_start = self.map_size[0]
         self.y_start = self.map_size[1]
         self.x_end = 0
         self.y_end = 0
-        self.resource_icon = Image.open(self.get_icon_path())
-        self.resource_icon = self.resource_icon.resize((int(150 * zoom), int(150 * zoom)))
+        resource_icon = Image.open(self.get_icon_path())
+        self.resource_icon = resource_icon.resize((int(150 * ZOOM), int(150 * ZOOM)))
         self.resource_xy_list = self.get_resource_point_list()
 
     def get_icon_path(self):
@@ -213,11 +255,11 @@ class ResourceMap:
 
     def get_resource_point_list(self):
         temp_list = []
-        for resource_point in data["all_resource_point_list"]:
+        for resource_point in self.all_resource_point_list:
             if str(resource_point["label_id"]) == self.resource_id:
                 # 获取xy坐标，然后加上中心点的坐标完成坐标转换
-                x = resource_point["x_pos"] + CENTER[0]  # noqa
-                y = resource_point["y_pos"] + CENTER[1]  # noqa
+                x = resource_point["x_pos"] + self.center[0]
+                y = resource_point["y_pos"] + self.center[1]
                 temp_list.append((int(x), int(y)))
         return temp_list
 
@@ -227,7 +269,7 @@ class ResourceMap:
             # 这时地图已经裁切过了，要以裁切后的地图左上角为中心再转换一次坐标
             x -= self.x_start
             y -= self.y_start
-            self.map_image.paste(self.resource_icon, (x + resource_icon_offset[0], y + resource_icon_offset[1]),
+            self.map_image.paste(self.resource_icon, (x + RESOURCE_ICON_OFFSET[0], y + RESOURCE_ICON_OFFSET[1]),
                                  self.resource_icon)
 
     def crop(self):
@@ -271,41 +313,3 @@ class ResourceMap:
 
     def get_resource_count(self):
         return len(self.resource_xy_list)
-
-
-async def get_resource_map_mes(name):
-    if data["date"] != time.strftime("%d"):
-        await init_point_list_and_map()
-    if name not in data["can_query_type_list"]:
-        return f"派蒙还不知道 {name} 在哪里呢，可以发送 `/map list` 查看资源列表"
-    map_res = ResourceMap(name)
-    count = map_res.get_resource_count()
-    if not count:
-        return f"派蒙没有找到 {name} 的位置，可能米游社wiki还没更新"
-    map_res.gen_jpg()
-    mes = f"派蒙一共找到 {name} 的 {count} 个位置点\n* 数据来源于米游社wiki"
-
-    return mes
-
-
-def get_resource_list_mes():
-    temp = {}
-    for list_id in data["all_resource_type"]:
-        # 先找1级目录
-        if data["all_resource_type"][list_id]["depth"] == 1:
-            temp[list_id] = []
-    for list_id in data["all_resource_type"]:
-        # 再找2级目录
-        if data["all_resource_type"][list_id]["depth"] == 2:
-            temp[str(data["all_resource_type"][list_id]["parent_id"])].append(list_id)
-    mes = "当前资源列表如下：\n"
-
-    for resource_type_id in temp:
-        if resource_type_id in ["1", "12", "50", "51", "95", "131"]:
-            # 在游戏里能查到的数据这里就不列举了，不然消息太长了
-            continue
-        mes += f"{data['all_resource_type'][resource_type_id]['name']}："
-        for resource_id in temp[resource_type_id]:
-            mes += f"{data['all_resource_type'][resource_id]['name']}，"
-        mes += "\n"
-    return mes
