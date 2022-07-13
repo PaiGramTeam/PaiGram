f79d8f7ce08b27811fd114af6bd5efc1d5c2e0eb
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-07-13 21:31:36 +0800

    修复 `gacha` 特殊情况下默认无输出的问题

--- service/gacha.py
+++ service/gacha.py
@@ -9,11 +9,11 @@ class GachaService:
         self.cache = cache
         self.gacha = GachaInfo()
 
-    async def gacha_info(self, gacha_name: str = "角色活动"):
+    async def gacha_info(self, gacha_name: str = "角色活动", default: bool = False):
         gacha_list_info = await self.gacha.get_gacha_list_info()
         gacha_id = ""
         for gacha in gacha_list_info.data["list"]:
-            if gacha["gacha_name"] == gacha_name:
+            if gacha["gacha_name"] == gacha_name or default:
                 gacha_id = gacha["gacha_id"]
         if gacha_id == "":
             return {}
