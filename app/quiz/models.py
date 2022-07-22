095ff0815f33f6f304cf7281849e4e04e49c6a10
洛水居室 <luoshuijs@outlook.com>
2022-07-23 01:49:54 +0800

    📝添加注释

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -6,6 +6,13 @@ from model.types import JSONDict
 
 class Answer(BaseObject):
     def __init__(self, answer_id: int = 0, question_id: int = 0, is_correct: bool = True, text: str = ""):
+        """Answer类
+
+        :param answer_id: 答案ID
+        :param question_id: 与之对应的问题ID
+        :param is_correct: 该答案是否正确
+        :param text: 答案文本
+        """
         self.answer_id = answer_id
         self.question_id = question_id
         self.text = text
@@ -16,6 +23,12 @@ class Answer(BaseObject):
 
 class Question(BaseObject):
     def __init__(self, question_id: int = 0, text: str = "", answers: List[Answer] = None):
+        """Question类
+
+        :param question_id: 问题ID
+        :param text: 问题文本
+        :param answers: 答案列表
+        """
         self.question_id = question_id
         self.text = text
         self.answers = [] if answers is None else answers
