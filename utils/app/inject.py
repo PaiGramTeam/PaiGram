ed5da75c00dac7d1121ac84c537e807176714cf4
洛水居室 <luoshuijs@outlook.com>
2022-07-31 01:28:10 +0800

    🔧 依赖注入支持同步函数注入

--- utils/app/inject.py
+++ utils/app/inject.py
@@ -6,28 +6,43 @@ from model.types import Func
 from utils.app.manager import ServiceDict
 
 
+def get_injections(func: Func):
+    injections = {}
+    try:
+        signature = inspect.signature(func)
+    except ValueError as exception:
+        if "no signature found" in str(exception):
+            Log.warning("no signature found", exception)
+        elif "not supported by signature" in str(exception):
+            Log.warning("not supported by signature", exception)
+        else:
+            raise exception
+    else:
+        for parameter_name, parameter in signature.parameters.items():
+            annotation = parameter.annotation
+            class_name = annotation.__name__
+            param = ServiceDict.get(class_name)
+            if param is not None:
+                injections.setdefault(parameter_name, param)
+    return injections
+
+
 def inject(func: Func) -> Func:
     """依赖注入"""
 
     @wraps(func)
-    async def decorator(*args, **kwargs):
-        try:
-            signature = inspect.signature(func)
-        except ValueError as exception:
-            if "no signature found" in str(exception):
-                Log.warning("no signature found", exception)
-            elif "not supported by signature" in str(exception):
-                Log.warning("not supported by signature", exception)
-            else:
-                raise exception
-        else:
-            for parameter_name, parameter in signature.parameters.items():
-                annotation = parameter.annotation
-                class_name = annotation.__name__
-                param = ServiceDict.get(class_name)
-                if param is not None:
-                    kwargs.setdefault(parameter_name, param)
-
+    async def async_decorator(*args, **kwargs):
+        injections = get_injections(func)
+        kwargs.update(injections)
         return await func(*args, **kwargs)
 
-    return decorator
+    @wraps(func)
+    def sync_decorator(*args, **kwargs):
+        injections = get_injections(func)
+        kwargs.update(injections)
+        return func(*args, **kwargs)
+
+    if inspect.iscoroutinefunction(func):
+        return async_decorator
+    else:
+        return sync_decorator
