6dccd29eeb428cd0707c20ab95c70016c28a0b21
Li Chuangbo <im@chuangbo.li>
2022-09-01 18:02:17 +0800

    ✨ 使用 alembic 管理数据库版本

---
+++ alembic/env.py
@@ -0,0 +1,115 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from sqlmodel import SQLModel
+
+from alembic import context
+
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+
+# target_metadata = mymodel.Base.metadata
+# target_metadata = None
+
+# register our models for alembic to auto-generate migrations
+from utils.manager import ModulesManager
+
+manager = ModulesManager()
+manager.refresh_list("core/*")
+manager.refresh_list("jobs/*")
+manager.refresh_list("plugins/genshin/*")
+manager.refresh_list("plugins/system/*")
+manager.import_module()
+
+target_metadata = SQLModel.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+# here we allow ourselves to pass interpolation vars to alembic.ini
+# from the application config module
+from config import config as appConfig
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", appConfig.mysql["host"])
+config.set_section_option(section, "DB_PORT", str(appConfig.mysql["port"]))
+config.set_section_option(section, "DB_USERNAME", appConfig.mysql["user"])
+config.set_section_option(section, "DB_PASSWORD", appConfig.mysql["password"])
+config.set_section_option(section, "DB_DATABASE", appConfig.mysql["database"])
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = AsyncEngine(
+        engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+            future=True,
+        )
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
