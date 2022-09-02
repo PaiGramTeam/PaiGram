8efb344aa6ed80de3c5a07dff9cbb7a586190daf
Karako <karakohear@gmail.com>
2022-09-02 14:06:29 +0800

    ✨ 适配一些 handler 1. CallbackQueryHandler 2. ChatJoinRequestHandler 3. ChatMemberHandler 4. ChosenInlineResultHandler 5. CommandHandler

--- core/app.py
+++ core/app.py
@@ -3,10 +3,11 @@ from multiprocessing import RLock as Lock
 from pathlib import Path
 from typing import Any, Callable, ClassVar, List, NoReturn, Optional, Type
 
-from telegram.ext import Application as TgApplication
+import pytz
+from telegram.ext import Application as TgApplication, Defaults
 
 from core.config import AppConfig
-from core.plugin import Plugin
+from core.plugin import _Plugin as Plugin
 from utils.const import PLUGIN_DIR, PROJECT_ROOT
 from utils.log import logger
 from utils.typed import StrOrPath
@@ -20,7 +21,7 @@ class Application(object):
 
     def __new__(cls, *args, **kwargs) -> "Application":
         """实现单例"""
-        with cls._lock:
+        with cls._lock:  # 使线程、进程安全
             if cls._instance is None:
                 cls._instance = object.__new__(cls)
         return cls._instance
@@ -31,33 +32,36 @@ class Application(object):
     _plugin_init_funcs: List[Callable[[TgApplication], Any]] = []
 
     def _instantiate_plugin(self, plugin: Type[Plugin]) -> Plugin:
-        """用于实例化Plugin的方法"""
+        """用于实例化Plugin的方法。用于给插件传入一些必要组件，如 MySQL、Redis等"""
         return plugin()
 
     def install_plugins(self, plugin_path: Optional[StrOrPath] = None):
+        """安装插件"""
         if plugin_path is None:
             plugin_path = PLUGIN_DIR
         else:
             plugin_path = Path(plugin_path).resolve()
 
-        for path in plugin_path.iterdir():
+        for path in plugin_path.iterdir():  # 遍历插件所在的目录
             if not path.name.startswith('_'):
                 if path.is_dir():
                     pkg = str(path.relative_to(PROJECT_ROOT))
                 else:
                     pkg = str(path.relative_to(PROJECT_ROOT)).split('.')[0]
                 try:
-                    module = import_module(pkg.replace('\\', '.'))
+                    module = import_module(pkg := pkg.replace('\\', '.'))  # 导入插件
                 except Exception as e:
-                    logger.exception(e)
-                    continue
+                    logger.error(
+                        f'在导入插件 "{pkg}" 的过程中遇到了错误：\n'
+                        f"[bold red]{type(e).__name__}: {e}[/]", extra={"markup": True}
+                    )
+                    continue  # 如有错误则继续
                 for attr in dir(module):
+                    # 找到该插件
                     if isinstance(cls := getattr(module, attr), type) and issubclass(cls, Plugin) and cls != Plugin:
-                        instance: Plugin = self._instantiate_plugin(cls)
-                        for data in instance.handler_datas():
-                            func = getattr(instance, data.pop('func'))
-                            self.app.add_handler(data.pop('type')(callback=func, **data))
-                        if hasattr(instance, 'init'):
+                        instance: Plugin = self._instantiate_plugin(cls)  # 实列化插件
+                        self.app.add_handlers(instance.handlers)  # 注册它的 handler
+                        if hasattr(instance, 'init'):  # 如果有 init 函数 则将这个函数添加至 post_init
                             self._plugin_init_funcs.append(getattr(instance, 'init'))
 
     async def _post_init(self, app: TgApplication) -> NoReturn:
@@ -65,10 +69,16 @@ class Application(object):
             try:
                 await func(app)
             except Exception as e:
-                breakpoint()
+                logger.exception(e)
 
     def launch(self) -> NoReturn:
-        self.app = TgApplication.builder().token(self.config.bot_token).post_init(self._post_init).build()
+        self.app = (
+            TgApplication.builder()
+            .defaults(Defaults(tzinfo=pytz.timezone("Asia/Shanghai")))
+            .token(self.config.bot_token)
+            .post_init(self._post_init)
+            .build()
+        )
         self.install_plugins()
         self.app.run_polling()
 
--- core/plugin.py
+++ core/plugin.py
@@ -1,75 +1,113 @@
-import inspect
-from typing import Callable, ClassVar, Dict, List, NoReturn, Optional, TYPE_CHECKING, TypeVar
+from importlib import import_module
+from re import Pattern
+from typing import Callable, Dict, List, Optional, Type, TypeVar, Union
 
 # noinspection PyProtectedMember
 from telegram._utils.defaultvalue import DEFAULT_TRUE
 # noinspection PyProtectedMember
 from telegram._utils.types import DVInput
-from telegram.ext import BaseHandler, CommandHandler
-# noinspection PyProtectedMember
+from telegram.ext import BaseHandler
 from telegram.ext.filters import BaseFilter
-from typing_extensions import ParamSpec, Self
+from typing_extensions import ParamSpec
 
-if TYPE_CHECKING:
-    from telegram.ext import Application
+__all__ = [
+    'Plugin', 'handler', 'conversation'
+]
 
 P = ParamSpec('P')
 T = TypeVar('T')
 HandlerType = TypeVar('HandlerType', bound=BaseHandler)
+Module = import_module('telegram.ext')
 
 
-class Plugin(object):
-    _instance: ClassVar[Optional[Self]] = None
+class _Plugin(object):
+    _handlers: List[HandlerType] = None
 
-    async def init(self, app: 'Application') -> NoReturn:
-        """当app刚启动时，需要运行的方法"""
+    def _make_handler(self, data: Dict) -> HandlerType:
+        func = getattr(self, data.pop('func'))
+        return data.pop('type')(callback=func, **data.pop('kwargs'))
 
-    @classmethod
-    def handler_datas(cls) -> List[Dict]:
-        result = []
-        for key in dir(cls):
-            # noinspection PyUnboundLocalVariable
-            if (
-                    not key.startswith('_')
-                    and
-                    isinstance(attr := getattr(cls, key), Callable)
-                    and
-                    (kwargs := getattr(attr, 'handler_kwargs', None))
-            ):
-                result.append(kwargs)
-        return result
+    @property
+    def handlers(self) -> List[HandlerType]:
+        if self._handlers is None:
+            self._handlers = []
+            for attr in dir(self):
+                if (
+                        not attr.startswith('_')
+                        and
+                        isinstance(attr := getattr(self, attr), Callable)
+                        and
+                        (data := getattr(attr, 'handler_data', None))
+                ):
+                    self._handlers.append(self._make_handler(data))
+        return self._handlers
 
 
-class _Command(object):
-    def __init__(
-            self,
-            command: str,
-            filters: BaseFilter = None,
-            block: DVInput[bool] = DEFAULT_TRUE
-    ):
-        self.kwargs = {'type': CommandHandler, 'command': command, 'filters': filters, 'block': block}
+class _Conversation(_Plugin):
+    ...
 
-    def __call__(self, func: Callable):
-        self.kwargs.update({'func': func.__name__})
-        setattr(func, 'handler_kwargs', self.kwargs)
+
+class Plugin(_Plugin):
+    Conversation = _Conversation
+
+
+class _Handler(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    @property
+    def type(self) -> Type[BaseHandler]:
+        return getattr(Module, f"{self.__class__.__name__.strip('_')}Handler")
+
+    def __call__(self, func: Callable[P, T]) -> Callable[P, T]:
+        setattr(func, 'handler_data', {'type': self.type, 'func': func.__name__, 'kwargs': self.kwargs})
         return func
 
 
+class _CallbackQuery(_Handler):
+    def __init__(
+            self,
+            pattern: Union[str, Pattern, type, Callable[[object], Optional[bool]]] = None,
+            block: DVInput[bool] = DEFAULT_TRUE,
+    ):
+        super(_CallbackQuery, self).__init__(pattern=pattern, block=block)
+
+
+class _ChatJoinRequest(_Handler):
+    def __init__(self, block: DVInput[bool] = DEFAULT_TRUE):
+        super(_ChatJoinRequest, self).__init__(block=block)
+
+
+class _ChatMember(_Handler):
+    def __init__(self, chat_member_types: int = -1):
+        super().__init__(chat_member_types=chat_member_types)
+
+
+class _ChosenInlineResult(_Handler):
+    def __init__(self, block: DVInput[bool] = DEFAULT_TRUE, pattern: Union[str, Pattern] = None):
+        super().__init__(block=block, pattern=pattern)
+
+
+class _Command(_Handler):
+    def __init__(self, command: str, filters: BaseFilter = None, block: DVInput[bool] = DEFAULT_TRUE):
+        super(_Command, self).__init__(command=command, filters=filters, block=block)
+
+
 # noinspection PyPep8Naming
-class handler(object):
-    def __init__(self, handler_type: Callable[P, HandlerType], *args: P.args, **kwargs: P.kwargs):
-        self._handler_type = handler_type
-        self._args = args
-        self._kwargs = kwargs
+class handler(_Handler):
+    def __init__(self, handler_type: Callable[P, HandlerType], **kwargs: P.kwargs):
+        self._type = handler_type
+        super(handler, self).__init__(**kwargs)
 
-    def __call__(self, func: Callable, *args, **kwargs) -> HandlerType:
-        from telegram.ext import Updater, CallbackContext
+    @property
+    def type(self) -> Type[BaseHandler]:
+        # noinspection PyTypeChecker
+        return self._type
 
-        def callback(updater: Updater, context: CallbackContext):
-            signature = inspect.signature(func)
-            breakpoint()
-            return func(self, updater, context)
-
-        return self._handler_type(*self._args, callback=callback, **self._kwargs)
-
+    callback_query = _CallbackQuery
+    chat_join_request = _ChatJoinRequest
     command = _Command
+
+
+class conversation(_Handler):
+    pass
