ba0460553f3385b5d060e640664973fa2babb05f
洛水居室 <luoshuijs@outlook.com>
2022-09-08 09:00:11 +0800

    ❌ 移除 `PlayerCards` 模块 等待后续更新重新添加

--- modules/playercards/models/fetter.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class FetterInfo(BaseModel):
-    """好感度信息"""
-    level: int = 0  # 好感度等级
--- modules/playercards/models/skill.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Skill(BaseModel):
-    """技能信息"""
-    skill_id: int = 0  # 技能ID
-    name: str = ""  # 技能名称
-    level: int = 0  # 技能等级
-    icon: str = ""  # 技能图标
