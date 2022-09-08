e3ca8466d4a2389d52004e0afba2e44494c08156
Li Chuangbo <im@chuangbo.li>
2022-09-08 15:00:14 +0800

    ✏️ 使用 `black` 代码格式化

--- plugins/genshin/player_cards.py
+++ plugins/genshin/player_cards.py
@@ -1,8 +1,16 @@
 import json
 from typing import Any, Dict, Union, Optional, List, cast
 
-from enkanetwork import EnkaNetworkAPI, Equipments, EquipmentsType, DigitType, Stats, CharacterInfo, \
-    EnkaNetworkResponse, Assets
+from enkanetwork import (
+    EnkaNetworkAPI,
+    Equipments,
+    EquipmentsType,
+    DigitType,
+    Stats,
+    CharacterInfo,
+    EnkaNetworkResponse,
+    Assets,
+)
 from telegram import Update
 from telegram.ext import CommandHandler, filters, CallbackContext, MessageHandler
 
@@ -22,7 +30,6 @@ assets = Assets(lang="chs")
 
 
 class PlayerCardsCache:
-
     def __init__(self, redis: RedisDB):
         self.question_qname = "player_cards"
         self.client = redis.client
@@ -39,8 +46,12 @@ class PlayerCardsCache:
 
 
 class PlayerCards(Plugin, BasePlugin):
-    def __init__(self, user_service: UserService = None, template_service: TemplateService = None,
-                 redis: RedisDB = None):
+    def __init__(
+        self,
+        user_service: UserService = None,
+        template_service: TemplateService = None,
+        redis: RedisDB = None,
+    ):
         self.user_service = user_service
         self.client = EnkaNetworkAPI(lang="chs")
         self.template_service = template_service
@@ -63,8 +74,12 @@ class PlayerCards(Plugin, BasePlugin):
         except UserNotFoundError:
             reply_message = await message.reply_text("未查询到账号信息，请先私聊派蒙绑定账号")
             if filters.ChatType.GROUPS.filter(message):
-                self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id, 30)
-                self._add_delete_message_job(context, message.chat_id, message.message_id, 30)
+                self._add_delete_message_job(
+                    context, reply_message.chat_id, reply_message.message_id, 30
+                )
+                self._add_delete_message_job(
+                    context, message.chat_id, message.message_id, 30
+                )
             return
         # 注意！！！！！！！！！！！！！！！！！！！！！！！！！！！！
         # 调试使用 非常建议手动缓存到cache 因为enka时不时会奔溃
@@ -78,18 +93,23 @@ class PlayerCards(Plugin, BasePlugin):
 
 
 class RenderTemplate:
-    def __init__(self, uid: Union[int, str], character: CharacterInfo, template_service: TemplateService = None):
+    def __init__(
+        self,
+        uid: Union[int, str],
+        character: CharacterInfo,
+        template_service: TemplateService = None,
+    ):
         self.uid = uid
         self.template_service = template_service
         self.character = character
 
     async def render(self):
-        player_card_data = {
-            "infos": []
-        }
+        player_card_data = {"infos": []}
         background = f"img/bg-{self.character.element.name.lower()}.jpg"
         player_card_data["background"] = background
-        player_card_data["image_banner"] = await url_to_file(self.character.image.banner.url)
+        player_card_data["image_banner"] = await url_to_file(
+            self.character.image.banner.url
+        )
         player_card_data["friendship_level"] = self.character.friendship_level
         player_card_data["name"] = self.character.name
         player_card_data["level"] = self.character.level
@@ -101,29 +121,46 @@ class RenderTemplate:
         # artifacts = await self.de_artifacts()
         # for artifact in artifacts:
         #     player_card_data["infos"].append(artifact)
-        data = await self.template_service.render_async('genshin/player_card', "player_card.html", player_card_data)
+        data = await self.template_service.render_async(
+            "genshin/player_card", "player_card.html", player_card_data
+        )
         print(data)
-        return await self.template_service.render('genshin/player_card', "player_card.html", player_card_data,
-                                                  {"width": 845, 'height': 1080}, full_page=True)
+        return await self.template_service.render(
+            "genshin/player_card",
+            "player_card.html",
+            player_card_data,
+            {"width": 845, "height": 1080},
+            full_page=True,
+        )
 
     async def de_stats(self) -> str:
+        stats = self.character.stats
         items: List[Dict[str, str]] = []
         logger.debug(self.character.stats)
 
-        items.append(("基础生命值", self.character.stats.BASE_HP.to_rounded()))
-
-        items.append(("生命值", self.character.stats.FIGHT_PROP_MAX_HP.to_rounded()))
-        items.append(("基础攻击力", self.character.stats.FIGHT_PROP_BASE_ATTACK.to_rounded()))
-        items.append(("攻击力", self.character.stats.FIGHT_PROP_CUR_ATTACK.to_rounded()))
-        items.append(("基础防御力", self.character.stats.FIGHT_PROP_BASE_DEFENSE.to_rounded()))
-        items.append(("防御力", self.character.stats.FIGHT_PROP_CUR_DEFENSE.to_rounded()))
-        items.append(("暴击率", self.character.stats.FIGHT_PROP_CRITICAL.to_percentage_symbol()))
-        items.append(("暴击伤害", self.character.stats.FIGHT_PROP_CRITICAL_HURT.to_percentage_symbol()))
-        items.append(("元素充能效率", self.character.stats.FIGHT_PROP_CHARGE_EFFICIENCY.to_percentage_symbol()))
-        items.append(("元素精通", self.character.stats.FIGHT_PROP_ELEMENT_MASTERY.to_rounded()))
+        items.append(("基础生命值", stats.BASE_HP.to_rounded()))
+        items.append(("生命值", stats.FIGHT_PROP_MAX_HP.to_rounded()))
+        items.append(("基础攻击力", stats.FIGHT_PROP_BASE_ATTACK.to_rounded()))
+        items.append(("攻击力", stats.FIGHT_PROP_CUR_ATTACK.to_rounded()))
+        items.append(("基础防御力", stats.FIGHT_PROP_BASE_DEFENSE.to_rounded()))
+        items.append(("防御力", stats.FIGHT_PROP_CUR_DEFENSE.to_rounded()))
+        items.append(("暴击率", stats.FIGHT_PROP_CRITICAL.to_percentage_symbol()))
+        items.append(
+            (
+                "暴击伤害",
+                stats.FIGHT_PROP_CRITICAL_HURT.to_percentage_symbol(),
+            )
+        )
+        items.append(
+            (
+                "元素充能效率",
+                stats.FIGHT_PROP_CHARGE_EFFICIENCY.to_percentage_symbol(),
+            )
+        )
+        items.append(("元素精通", stats.FIGHT_PROP_ELEMENT_MASTERY.to_rounded()))
 
         # 查找元素伤害加成和治疗加成
-        for stat in self.character.stats:
+        for stat in stats:
             if 40 <= stat[1].id <= 46:  # 元素伤害加成
                 pass
             elif stat[1].id == 29:  # 物理伤害加成
@@ -132,7 +169,11 @@ class RenderTemplate:
                 pass
             else:
                 continue
-            value = stat[1].to_rounded() if isinstance(stat[1], Stats) else stat[1].to_percentage_symbol()
+            value = (
+                stat[1].to_rounded()
+                if isinstance(stat[1], Stats)
+                else stat[1].to_percentage_symbol()
+            )
             if value == 0 or value == "0%":
                 continue
             name = assets.get_hash_map(stat[0])
@@ -140,7 +181,7 @@ class RenderTemplate:
                 continue
             logger.info(f"{name} -> {value}")
             items.append((name, value))
-        
+
         return items
 
     async def de_skills(self) -> str:
@@ -149,11 +190,7 @@ class RenderTemplate:
 
         for skill in self.character.skills:
             img = await url_to_file(skill.icon.url)
-            items.append({
-                "background": background,
-                "img": img,
-                "level": skill.level
-            })
+            items.append({"background": background, "img": img, "level": skill.level})
 
         return items
 
@@ -162,17 +199,21 @@ class RenderTemplate:
         items = []
 
         for constellation in self.character.constellations:
-            items.append({
-                "background": background,
-                "img": constellation.icon.url,
-                "unlocked": constellation.unlocked,
-            })
+            items.append(
+                {
+                    "background": background,
+                    "img": constellation.icon.url,
+                    "unlocked": constellation.unlocked,
+                }
+            )
 
         return items
 
     async def de_artifacts(self) -> List[str]:
         html_list: List[str] = []
-        for artifact in filter(lambda x: x.type == EquipmentsType.ARTIFACT, self.character.equipments):
+        for artifact in filter(
+            lambda x: x.type == EquipmentsType.ARTIFACT, self.character.equipments
+        ):
             artifact = cast(Equipments, artifact)
             img = await url_to_file(artifact.detail.icon.url)
             artifacts_data = {
@@ -181,17 +222,21 @@ class RenderTemplate:
                 "name": artifact.detail.name,
                 "detail_names": [],
                 "detail_values": [],
-                "detail_scores": []
+                "detail_scores": [],
             }
             artifacts_data["detail_names"].append(artifact.detail.mainstats.name)
             artifacts_data["detail_values"].append(
-                f"{artifact.detail.mainstats.value}{'%' if artifact.detail.mainstats.type == DigitType.PERCENT else ''}")
+                f"{artifact.detail.mainstats.value}{'%' if artifact.detail.mainstats.type == DigitType.PERCENT else ''}"
+            )
             artifacts_data["detail_scores"].append("-")
             for substate in artifact.detail.substats:
                 artifacts_data["detail_names"].append(substate.name)
                 artifacts_data["detail_values"].append(
-                    f"{substate.value}{'%' if substate.type == DigitType.PERCENT else ''}")
+                    f"{substate.value}{'%' if substate.type == DigitType.PERCENT else ''}"
+                )
                 artifacts_data["detail_scores"].append(substate)
-            html = await self.template_service.render_async('genshin/player_card', "artifacts.html", artifacts_data)
+            html = await self.template_service.render_async(
+                "genshin/player_card", "artifacts.html", artifacts_data
+            )
             html_list.append(html)
         return html_list
