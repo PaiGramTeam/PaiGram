2373d12cef656e24e41c9339074c6e24b38e7eee
xtaodada <xtao@xtaolink.cn>
2022-09-12 21:30:14 +0800

    🧪 添加圣遗物识图测试

--- tests/test_artifact.py
+++ tests/test_artifact.py
@@ -1,7 +1,10 @@
 import pytest
+import logging
 import pytest_asyncio
 
 from modules.apihelper.artifact import ArtifactOcrRate
+
+LOGGER = logging.getLogger(__name__)
 
 
 @pytest_asyncio.fixture
@@ -13,12 +16,22 @@ async def artifact_rate():
 
 # noinspection PyShadowingNames
 @pytest.mark.asyncio
-async def test_rate_artifact(artifact_rate):
-    artifact_attr = {
-        'name': '翠绿的猎人之冠', 'pos': '理之冠', 'star': 5, 'level': 20,
-        'main_item': {'type': 'cr', 'name': '暴击率', 'value': '31.1%'},
-        'sub_item': [{'type': 'hp', 'name': '生命值', 'value': '9.3%'},
-                     {'type': 'df', 'name': '防御力', 'value': '46'},
-                     {'type': 'atk', 'name': '攻击力', 'value': '49'},
-                     {'type': 'cd', 'name': '暴击伤害', 'value': '10.9%'}]}
-    assert await artifact_rate.rate_artifact(artifact_attr)
+class TestArtifactOcrRate:
+    @staticmethod
+    async def test_rate_artifact(artifact_rate):
+        artifact_attr = {
+            'name': '翠绿的猎人之冠', 'pos': '理之冠', 'star': 5, 'level': 20,
+            'main_item': {'type': 'cr', 'name': '暴击率', 'value': '31.1%'},
+            'sub_item': [{'type': 'hp', 'name': '生命值', 'value': '9.3%'},
+                         {'type': 'df', 'name': '防御力', 'value': '46'},
+                         {'type': 'atk', 'name': '攻击力', 'value': '49'},
+                         {'type': 'cd', 'name': '暴击伤害', 'value': '10.9%'}]}
+        assert await artifact_rate.rate_artifact(artifact_attr)
+
+    @staticmethod
+    async def test_ocr_artifact(artifact_rate):
+        with open("tests/data/test_artifact.jpg", "rb") as f:
+            photo = f.read()
+        data = await artifact_rate.get_artifact_attr(photo)
+        LOGGER.info(data.text)
+        assert data.status_code == 200
