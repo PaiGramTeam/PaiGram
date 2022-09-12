a5d5d0199043e1e5c8a3c2c5b91e682db2a507a4
洛水居室 <luoshuijs@outlook.com>
2022-09-12 15:50:39 +0800

    🎨 添加 `event_loop`

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -1,11 +1,24 @@
+import asyncio
+
+import pytest
+
 from modules.wiki.character import Character
 from modules.wiki.material import Material
 from modules.wiki.weapon import Weapon
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.mark.asyncio
 class TestWeapon:
 
-    async def test_get_by_id(self):
+    @staticmethod
+    async def test_get_by_id():
         weapon = await Weapon.get_by_id('11417')
         assert weapon.name == '原木刀'
         assert weapon.rarity == 4
@@ -13,7 +26,8 @@ class TestWeapon:
         assert weapon.attribute.type.value == '元素充能效率'
         assert weapon.affix.name == '森林的瑞佑'
 
-    async def test_get_by_name(self):
+    @staticmethod
+    async def test_get_by_name():
         weapon = await Weapon.get_by_name('风鹰剑')
         assert weapon.id == 11501
         assert weapon.rarity == 5
@@ -22,22 +36,26 @@ class TestWeapon:
         assert weapon.affix.name == '西风之鹰的抗争'
         assert '听凭风引，便是正义与自由之风' in weapon.story
 
-    async def test_get_full_gen(self):
+    @staticmethod
+    async def test_get_full_gen():
         async for weapon in Weapon.full_data_generator():
             assert isinstance(weapon, Weapon)
 
-    async def test_get_full(self):
+    @staticmethod
+    async def test_get_full():
         full_data = await Weapon.get_full_data()
         for weapon in full_data:
             assert isinstance(weapon, Weapon)
 
-    async def test_name_list(self):
+    @staticmethod
+    async def test_name_list():
         from httpx import URL
         async for name in Weapon._name_list_generator(with_url=True):
             assert isinstance(name[0], str)
             assert isinstance(name[1], URL)
 
 
+@pytest.mark.asyncio
 class TestCharacter:
     async def test_get_by_id(self):
         character = await Character.get_by_id('ayaka_002')
@@ -64,20 +82,26 @@ class TestCharacter:
             assert isinstance(character, Character)
 
 
+@pytest.mark.asyncio
 class TestMaterial:
-    async def test_get_full_gen(self):
+
+    @staticmethod
+    async def test_get_full_gen():
         async for material in Material.full_data_generator():
             assert isinstance(material, Material)
 
-    async def test_get_full(self):
+    @staticmethod
+    async def test_get_full():
         material_list = await Material.get_full_data()
         for material in material_list:
             assert isinstance(material, Material)
 
 
+@pytest.mark.asyncio
 class TestAll:
-    async def test_all_get_full(self):
-        import asyncio
+
+    @staticmethod
+    async def test_all_get_full():
         materials, weapons, characters = tuple(await asyncio.gather(
             Material.get_full_data(),
             Weapon.get_full_data(),
