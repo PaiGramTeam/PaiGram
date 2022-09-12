50de1f35550c0837036b54a9396b28635b0ad639
洛水居室 <luoshuijs@outlook.com>
2022-09-12 23:18:11 +0800

    ♻ 重写单元测试
    默认测试运行程序从 `unittest`  改为 `pytest`
    
    Co-authored-by: xtaodada <xtao@xtaolink.cn>
    Co-authored-by: Karako <karakohear@gmail.com>

---
+++ tests/test_artifact.py
@@ -0,0 +1,42 @@
+import logging
+
+import pytest
+import pytest_asyncio
+from flaky import flaky
+
+from modules.apihelper.artifact import ArtifactOcrRate
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest_asyncio.fixture
+async def artifact_rate():
+    _artifact_rate = ArtifactOcrRate()
+    yield _artifact_rate
+    await _artifact_rate.close()
+
+
+# noinspection PyShadowingNames
+@pytest.mark.asyncio
+class TestArtifactOcrRate:
+
+    @staticmethod
+    @flaky(3, 1)
+    async def test_rate_artifact(artifact_rate):
+        artifact_attr = {
+            'name': '翠绿的猎人之冠', 'pos': '理之冠', 'star': 5, 'level': 20,
+            'main_item': {'type': 'cr', 'name': '暴击率', 'value': '31.1%'},
+            'sub_item': [{'type': 'hp', 'name': '生命值', 'value': '9.3%'},
+                         {'type': 'df', 'name': '防御力', 'value': '46'},
+                         {'type': 'atk', 'name': '攻击力', 'value': '49'},
+                         {'type': 'cd', 'name': '暴击伤害', 'value': '10.9%'}]}
+        assert await artifact_rate.rate_artifact(artifact_attr)
+
+    @staticmethod
+    @flaky(3, 1)
+    async def test_ocr_artifact(artifact_rate):
+        with open("tests/data/test_artifact.jpg", "rb") as f:
+            photo = f.read()
+        data = await artifact_rate.get_artifact_attr(photo)
+        LOGGER.info(data.text)
+        assert data.status_code == 200
---
+++ tests/test_game.py
@@ -0,0 +1,36 @@
+import pytest
+import pytest_asyncio
+from flaky import flaky
+
+from modules.apihelper.hyperion import Hyperion
+
+
+@pytest_asyncio.fixture
+async def hyperion():
+    _hyperion = Hyperion()
+    yield _hyperion
+    await _hyperion.close()
+
+
+# noinspection PyShadowingNames
+@pytest.mark.asyncio
+@flaky(3, 1)
+async def test_get_strategy(hyperion):
+    test_collection_id_list = [839176, 839179, 839181]
+    test_result = ["温迪", "胡桃", "雷电将军"]
+
+    async def get_post_id(_collection_id: int, character_name: str) -> str:
+        post_full_in_collection = await hyperion.get_post_full_in_collection(_collection_id)
+        if post_full_in_collection.error:
+            raise RuntimeError(f"获取收藏信息错误，错误信息为：{post_full_in_collection.message}")
+        for post_data in post_full_in_collection.data["posts"]:
+            topics = post_data["topics"]
+            for topic in topics:
+                if character_name == topic["name"]:
+                    return topic["name"]
+        return ""
+
+    for index, _ in enumerate(test_collection_id_list):
+        second = test_result[index]
+        first = await get_post_id(test_collection_id_list[index], second)
+        assert first == second
