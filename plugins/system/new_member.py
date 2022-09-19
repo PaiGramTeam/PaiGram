1aa0e3d38dbcdbd33e4c9eb8acb648d5c939c9c3
洛水居室 <luoshuijs@outlook.com>
2022-09-19 17:19:34 +0800

    🎨 改进 Bot 邀请验证

--- plugins/system/new_member.py
+++ plugins/system/new_member.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import CallbackContext, filters
+from telegram.ext import CallbackContext
 
 from core.admin import BotAdminService
 from core.plugin import Plugin, handler
@@ -17,20 +17,19 @@ class BotJoiningGroupsVerification(Plugin):
         chat = message.chat
         from_user = message.from_user
         quit_status = False
-        if filters.ChatType.GROUPS.filter(message):
-            for user in message.new_chat_members:
-                if user.id == context.bot.id:
-                    if from_user is not None:
-                        logger.info(f"用户 {from_user.full_name}[{from_user.id}] 在群 {chat.title}[{chat.id}] 邀请BOT")
-                        admin_list = await self.bot_admin_service.get_admin_list()
-                        if from_user.id in admin_list:
-                            await context.bot.send_message(message.chat_id,
-                                                           '感谢邀请小派蒙到本群！请使用 /help 查看咱已经学会的功能。')
-                        else:
-                            quit_status = True
-                    else:
-                        logger.info(f"未知用户 在群 {chat.title}[{chat.id}] 邀请BOT")
-                        quit_status = True
+        for new_chat_members_user in message.new_chat_members:
+            if new_chat_members_user.id == context.bot.id:
+                logger.info(f"有人邀请BOT进入群 {chat.title}[{chat.id}]")
+                quit_status = True
+                if from_user is not None:
+                    logger.info(f"用户 {from_user.full_name}[{from_user.id}] 在群 {chat.title}[{chat.id}] 邀请BOT")
+                    admin_list = await self.bot_admin_service.get_admin_list()
+                    if from_user.id in admin_list:
+                        await context.bot.send_message(message.chat_id,
+                                                       '感谢邀请小派蒙到本群！请使用 /help 查看咱已经学会的功能。')
+                        quit_status = False
+                else:
+                    logger.info(f"未知用户 在群 {chat.title}[{chat.id}] 邀请BOT")
         if quit_status:
             logger.warning("不是管理员邀请！退出群聊。")
             await context.bot.send_message(message.chat_id, "派蒙不想进去！不是旅行者的邀请！")
