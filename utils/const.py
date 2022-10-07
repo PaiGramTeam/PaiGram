4432de4215e20028e21d922693693fc130a0eae9
Li Chuangbo <im@chuangbo.li>
2022-10-07 15:55:59 +0800

    🐛 修复 alembic 只在 core 和 plugins 目录查找 models
    因为有可能项目根目录有其他不在 git 里管理的目录存在 models.py 文件，比如
    `venv/Lib/site-packages/charset_normalizer/models.py`

--- utils/const.py
+++ utils/const.py
@@ -11,6 +11,8 @@ __all__ = [
 
 # 项目根目录
 PROJECT_ROOT = Path(__file__).joinpath('../..').resolve()
+# Core 目录
+CORE_DIR = PROJECT_ROOT / 'core'
 # 插件目录
 PLUGIN_DIR = PROJECT_ROOT / 'plugins'
 # 资源目录
