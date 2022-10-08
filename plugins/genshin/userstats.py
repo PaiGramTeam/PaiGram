aea5e41d7b9b092a49d79fb76a1cabe5e8aac483
洛水居室 <luoshuijs@outlook.com>
2022-10-08 20:47:55 +0800

    🎨 重写玩家统计
    Co-authored-by: Li Chuangbo <im@chuangbo.li>

--- plugins/genshin/userstats.py
+++ plugins/genshin/userstats.py
@@ -1,11 +1,17 @@
-import os
-import random
+import secrets
 from typing import Optional
 
 from genshin import GenshinException, Client
+from genshin.models import GenshinUserStats
 from telegram import Update
 from telegram.constants import ChatAction
-from telegram.ext import CallbackContext, CommandHandler, MessageHandler, ConversationHandler, filters
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    filters,
+)
 
 from core.baseplugin import BasePlugin
 from core.cookies.error import CookiesNotFoundError
@@ -27,86 +33,6 @@ class UserStatsPlugins(Plugin, BasePlugin):
 
     def __init__(self, template_service: TemplateService = None):
         self.template_service = template_service
-        self.current_dir = os.getcwd()
-
-    async def _start_get_user_info(self, client: Client, uid: int = -1) -> bytes:
-        if uid == -1:
-            _uid = client.uid
-        else:
-            _uid = uid
-        try:
-            user_info = await client.get_genshin_user(_uid)
-        except GenshinException as exc:
-            raise exc
-        if user_info.teapot is None:
-            raise TeapotUnlocked
-        nickname = user_info.info.nickname
-        user_uid = _uid
-        user_avatar = user_info.characters[0].icon
-        user_data = {
-            "name": nickname,
-            "uid": user_uid,
-            "user_avatar": await url_to_file(user_avatar),
-            "action_day_number": user_info.stats.days_active,
-            "achievement_number": user_info.stats.achievements,
-            "avatar_number": user_info.stats.characters,
-            "spiral_abyss": user_info.stats.spiral_abyss,
-            "way_point_number": user_info.stats.unlocked_waypoints,
-            "domain_number": user_info.stats.unlocked_domains,
-            "luxurious_number": user_info.stats.luxurious_chests,
-            "precious_chest_number": user_info.stats.precious_chests,
-            "exquisite_chest_number": user_info.stats.exquisite_chests,
-            "common_chest_number": user_info.stats.common_chests,
-            "magic_chest_number": user_info.stats.remarkable_chests,
-            "anemoculus_number": user_info.stats.anemoculi,
-            "geoculus_number": user_info.stats.geoculi,
-            "electroculus_number": user_info.stats.electroculi,
-            "dendroculi_number": user_info.stats.dendroculi,
-            "world_exploration_list": [],
-            "teapot_level": user_info.teapot.level,
-            "teapot_comfort_num": user_info.teapot.comfort,
-            "teapot_item_num": user_info.teapot.items,
-            "teapot_visit_num": user_info.teapot.visitors,
-            "teapot_list": []
-        }
-        for exploration in user_info.explorations:
-            exploration_data = {
-                "name": exploration.name,
-                "exploration_percentage": exploration.explored,
-                "offerings": [],
-                "icon": await url_to_file(exploration.icon)
-            }
-            for offering in exploration.offerings:
-                # 修复上游奇怪的问题
-                if isinstance(offering, dict):
-                    offering_name = offering["name"]
-                    offering_level = offering["level"]
-                else:
-                    offering_name = offering.name
-                    offering_level = offering.level
-                if offering_name == "Reputation":
-                    offering_name = "声望等级"
-                offering_data = {
-                    "data": f"{offering_name}：{offering_level}级"
-                }
-                exploration_data["offerings"].append(offering_data)
-            user_data["world_exploration_list"].append(exploration_data)
-        for teapot in user_info.teapot.realms:
-            teapot_icon = teapot.icon
-            # 修复 国际服绘绮庭 图标 地址请求 为404
-            if "UI_HomeworldModule_4_Pic.png" in teapot_icon:
-                teapot_icon = "https://upload-bbs.mihoyo.com/game_record/genshin/home/UI_HomeworldModule_4_Pic.png"
-            teapot_data = {
-                "icon": await url_to_file(teapot_icon),
-                "name": teapot.name
-            }
-            user_data["teapot_list"].append(teapot_data)
-        background_image = random.choice(os.listdir(f"{self.current_dir}/resources/background/vertical"))
-        user_data[
-            "background_image"] = f"file://{self.current_dir}/resources/background/vertical/{background_image}"
-        png_data = await self.template_service.render('genshin/info', "info.html", user_data,
-                                                      {"width": 1024, "height": 1024})
-        return png_data
 
     @handler(CommandHandler, command="stats", block=False)
     @handler(MessageHandler, filters=filters.Regex("^玩家统计查询(.*)"), block=False)
@@ -133,13 +59,17 @@ class UserStatsPlugins(Plugin, BasePlugin):
                 client, _uid = await get_public_genshin_client(user.id)
                 if uid == -1:
                     uid = _uid
-            png_data = await self._start_get_user_info(client, uid)
+            png_data = await self.render(client, uid)
         except UserNotFoundError:
             reply_message = await message.reply_text("未查询到账号信息，请先私聊派蒙绑定账号")
             if filters.ChatType.GROUPS.filter(message):
-                self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id, 30)
+                self._add_delete_message_job(
+                    context, reply_message.chat_id, reply_message.message_id, 30
+                )
 
-                self._add_delete_message_job(context, message.chat_id, message.message_id, 30)
+                self._add_delete_message_job(
+                    context, message.chat_id, message.message_id, 30
+                )
             return
         except TeapotUnlocked:
             await message.reply_text("角色尘歌壶未解锁 如果想要查看具体数据 嗯...... 咕咕咕~")
@@ -150,4 +80,71 @@ class UserStatsPlugins(Plugin, BasePlugin):
             await message.reply_text("角色数据有误 估计是派蒙晕了")
             return ConversationHandler.END
         await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
-        await message.reply_photo(png_data, filename=f"{client.uid}.png", allow_sending_without_reply=True)
+        await message.reply_photo(
+            png_data, filename=f"{client.uid}.png", allow_sending_without_reply=True
+        )
+
+    async def render(self, client: Client, uid: int = -1) -> bytes:
+        if uid == -1 and client.uid:
+            uid = client.uid
+
+        try:
+            user_info = await client.get_genshin_user(uid)
+            logger.debug(user_info)
+        except GenshinException as exc:
+            raise exc
+
+        # 因为需要替换线上图片地址为本地地址，先克隆数据，避免修改原数据
+        user_info = user_info.copy(deep=True)
+
+        data = {
+            "uid": uid,
+            "info": user_info.info,
+            "stats": user_info.stats,
+            "explorations": user_info.explorations,
+            "teapot": user_info.teapot,
+            "stats_labels": [
+                ("活跃天数", "days_active"),
+                ("成就达成数", "achievements"),
+                ("获取角色数", "characters"),
+                ("深境螺旋", "spiral_abyss"),
+                ("解锁传送点", "unlocked_waypoints"),
+                ("解锁秘境", "unlocked_domains"),
+                ("奇馈宝箱数", "remarkable_chests"),
+                ("华丽宝箱数", "luxurious_chests"),
+                ("珍贵宝箱数", "precious_chests"),
+                ("精致宝箱数", "exquisite_chests"),
+                ("普通宝箱数", "common_chests"),
+                ("风神瞳", "anemoculi"),
+                ("岩神瞳", "geoculi"),
+                ("雷神瞳", "electroculi"),
+                ("草神瞳", "dendroculi"),
+            ],
+            "style": secrets.choice(["mondstadt", "liyue"])
+        }
+
+        # html = await self.template_service.render_async(
+        #     "genshin/stats", "stats.html", data
+        # )
+        # logger.debug(html)
+
+        await self.cache_images(user_info)
+
+        return await self.template_service.render(
+            "genshin/stats",
+            "stats.html",
+            data,
+            {"width": 650, "height": 800},
+            full_page=True,
+        )
+
+    @staticmethod
+    async def cache_images(data: GenshinUserStats) -> None:
+        """缓存所有图片到本地"""
+        # TODO: 并发下载所有资源
+
+        # 探索地区
+        for item in data.explorations:
+            item.__config__.allow_mutation = True
+            item.icon = await url_to_file(item.icon)
+            item.cover = await url_to_file(item.cover)
