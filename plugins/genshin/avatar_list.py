fc526ca215a1644be6d911f3c1d0f140f2aaf78d
Karako <karakohear@gmail.com>
2022-10-15 22:19:31 +0800

    🚧 build
    Signed-off-by: Karako <karakohear@gmail.com>

--- plugins/genshin/avatar_list.py
+++ plugins/genshin/avatar_list.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import Iterable, List
 
 from arkowrapper import ArkoWrapper
 from enkanetwork import Assets as EnkaAssets, EnkaNetworkAPI
-from genshin.models import CalculatorCharacterDetails, Character
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.constants import ParseMode
+from genshin.models import CalculatorCharacterDetails, CalculatorTalent, Character
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputFile, Update
+from telegram.constants import ChatAction, ParseMode
 from telegram.ext import CallbackContext, filters
 
 from core.base.assets import AssetsService
@@ -13,6 +13,7 @@ from core.config import config
 from core.cookies.error import CookiesNotFoundError
 from core.cookies.services import CookiesService
 from core.plugin import Plugin, handler
+from core.template import TemplateService
 from core.user.error import UserNotFoundError
 from metadata.genshin import NAMECARD_DATA
 from modules.wiki.base import Model
@@ -25,9 +26,12 @@ api = "https://api-takumi.mihoyo.com/event/e20200928calculate/v1/sync/avatar/det
 
 
 class AvatarListPlugin(Plugin, BasePlugin):
-    def __init__(self, cookies_service: CookiesService, assets_service: AssetsService) -> None:
+    def __init__(
+        self, cookies_service: CookiesService, assets_service: AssetsService, template_service: TemplateService
+    ) -> None:
         self.cookies_service = cookies_service
         self.assets_service = assets_service
+        self.template_service = template_service
         self.enka_client = EnkaNetworkAPI(lang="chs", agent=config.enka_network_api_agent)
         self.enka_assets = EnkaAssets(lang="chs")
 
@@ -38,6 +42,8 @@ class AvatarListPlugin(Plugin, BasePlugin):
     async def avatar_list(self, update: Update, context: CallbackContext):
         user = update.effective_user
         message = update.effective_message
+
+        logger.info(f"用户 {user.full_name}[{user.id}] [bold]练度统计[/bold]", extra={"markup": True})
 
         try:
             client = await get_genshin_client(user.id)
@@ -66,9 +72,11 @@ class AvatarListPlugin(Plugin, BasePlugin):
                 await message.reply_text("此功能需要绑定<code>cookie</code>后使用，请先私聊派蒙进行绑定", parse_mode=ParseMode.HTML)
             return
 
+        await message.reply_chat_action(ChatAction.TYPING)
+
         characters = await client.get_genshin_characters(client.uid)
 
-        avatars: List[AvatarData] = []
+        avatar_datas: List[AvatarData] = []
         for character in characters:
             try:
                 detail = await client.get_character_details(character)
@@ -77,16 +85,25 @@ class AvatarListPlugin(Plugin, BasePlugin):
                 detail = CalculatorCharacterDetails.parse_obj(
                     await client.request(api, params={"uid": client.uid, "region": region, "avatar_id": character.id})
                 )
-            avatars.append(
+            talents = [t for t in detail.talents if t.type in ["attack", "skill", "burst"]]
+            buffed_talents = []
+            for constellation in filter(lambda x: x.pos in [3, 5], character.constellations[: character.constellation]):
+                if result := list(filter(lambda x: all([x.name in constellation.effect]), talents)):
+                    buffed_talents.append(result[0].type)
+            avatar_datas.append(
                 AvatarData(
-                    data=character,
+                    avatar=character,
                     detail=detail,
                     icon=(await self.assets_service.avatar(character.id).side()).as_uri(),
                     weapon=(
-                        await self.assets_service.weapon(character.weapon.id).__getattribute__(
+                        await self.assets_service.weapon(character.weapon.id).__getattr__(
                             "icon" if character.weapon.ascension < 2 else "awaken"
                         )()
                     ).as_uri(),
+                    skills=[
+                        SkillData(skill=s, buffed=s.type in buffed_talents)
+                        for s in sorted(talents, key=lambda x: ["attack", "skill", "burst"].index(x.type))
+                    ],
                 )
             )
 
@@ -115,12 +132,26 @@ class AvatarListPlugin(Plugin, BasePlugin):
             "nickname": nickname,
             "avatar": avatar,
             "namecard": namecard,
-            "data": AvatarData,
+            "avatar_datas": avatar_datas,
         }
+
+        await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+
+        image = await self.template_service.render(
+            "genshin/avatar_list/main.html", render_data, viewport={"width": 1040, "height": 500}, full_page=True
+        )
+
+        await message.reply_document(InputFile(image, filename=f"{user.full_name}的练度统计.png"))
+
+
+class SkillData(Model):
+    skill: CalculatorTalent
+    buffed: bool = False
 
 
 class AvatarData(Model):
-    data: Character
+    avatar: Character
     detail: CalculatorCharacterDetails
     icon: str
     weapon: str
+    skills: Iterable[SkillData]
