88a92ef4dc3fbb86fcfe64ff05c5785167ffe75f
xtaodada <xtao@xtaolink.cn>
2022-10-16 01:26:14 +0800

    ⚡️ 优化代码

--- plugins/genshin/avatar_list.py
+++ plugins/genshin/avatar_list.py
@@ -1,10 +1,11 @@
 """练度统计"""
-from typing import Iterable, List
+from typing import Iterable, List, Optional, Sequence
 
 from arkowrapper import ArkoWrapper
 from enkanetwork import Assets as EnkaAssets, EnkaNetworkAPI
+from genshin import Client
 from genshin.models import CalculatorCharacterDetails, CalculatorTalent, Character
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputFile, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputFile, Update, User, Message
 from telegram.constants import ChatAction, ParseMode
 from telegram.ext import CallbackContext, filters
 
@@ -23,8 +24,6 @@ from utils.decorators.restricts import restricts
 from utils.helpers import get_genshin_client
 from utils.log import logger
 
-api = "https://api-takumi.mihoyo.com/event/e20200928calculate/v1/sync/avatar/detail"
-
 
 class AvatarListPlugin(Plugin, BasePlugin):
     def __init__(
@@ -35,19 +34,11 @@ class AvatarListPlugin(Plugin, BasePlugin):
         self.template_service = template_service
         self.enka_client = EnkaNetworkAPI(lang="chs", agent=config.enka_network_api_agent)
         self.enka_assets = EnkaAssets(lang="chs")
+        self.api = "https://api-takumi.mihoyo.com/event/e20200928calculate/v1/sync/avatar/detail"
 
-    @handler.command("avatars")
-    @handler.message(filters.Regex(r"^练度统计$"))
-    @restricts(30)
-    @error_callable
-    async def avatar_list(self, update: Update, context: CallbackContext):
-        user = update.effective_user
-        message = update.effective_message
-
-        logger.info(f"用户 {user.full_name}[{user.id}] [bold]练度统计[/bold]", extra={"markup": True})
-
+    async def get_user_client(self, user: User, message: Message, context: CallbackContext) -> Optional[Client]:
         try:
-            client = await get_genshin_client(user.id)
+            return await get_genshin_client(user.id)
         except UserNotFoundError:  # 若未找到账号
             if filters.ChatType.GROUPS.filter(message):
                 buttons = [[InlineKeyboardButton("点我私聊", url=f"https://t.me/{context.bot.username}?start=set_uid")]]
@@ -58,7 +49,6 @@ class AvatarListPlugin(Plugin, BasePlugin):
                 self._add_delete_message_job(context, message.chat_id, message.message_id, 30)
             else:
                 await message.reply_text("未查询到您所绑定的账号信息，请先私聊派蒙绑定账号")
-            return
         except CookiesNotFoundError:
             if filters.ChatType.GROUPS.filter(message):
                 buttons = [[InlineKeyboardButton("点我私聊", url=f"https://t.me/{context.bot.username}?start=set_uid")]]
@@ -71,29 +61,28 @@ class AvatarListPlugin(Plugin, BasePlugin):
                 self._add_delete_message_job(context, message.chat_id, message.message_id, 30)
             else:
                 await message.reply_text("此功能需要绑定<code>cookie</code>后使用，请先私聊派蒙进行绑定", parse_mode=ParseMode.HTML)
-            return
 
-        notice = await message.reply_text("派蒙需要收集整理数据，还请耐心等待哦~")
-        await message.reply_chat_action(ChatAction.TYPING)
+    async def get_character_details(self, client: Client, character: Character) -> CalculatorCharacterDetails:
+        try:
+            detail = await client.get_character_details(character)
+        except RuntimeError:
+            region = "cn_gf01" if str(client.uid)[0] in ["1", "2"] else "cn_qd01"
+            detail = CalculatorCharacterDetails.parse_obj(
+                await client.request(self.api, params={"uid": client.uid, "region": region, "avatar_id": character.id})
+            )
+        return detail
 
-        characters = await client.get_genshin_characters(client.uid)
-
+    async def get_avatars_data(self, characters: Sequence[Character], client: Client, max_length: int = None):
         avatar_datas: List[AvatarData] = []
         for character in characters:
-            try:
-                detail = await client.get_character_details(character)
-            except RuntimeError:
-                region = "cn_gf01" if str(client.uid)[0] in ["1", "2"] else "cn_qd01"
-                detail = CalculatorCharacterDetails.parse_obj(
-                    await client.request(api, params={"uid": client.uid, "region": region, "avatar_id": character.id})
-                )
-            if character.id == 10000007:
+            detail = await self.get_character_details(client, character)
+            if character.id == 10000005:
                 talents = []
                 for talent in detail.talents:
                     if "普通攻击" in talent.name:
                         talent.Config.allow_mutation = True
                         # noinspection Pydantic
-                        talent.group_id = 1
+                        talent.group_id = 1131
                     if talent.type in ["attack", "skill", "burst"]:
                         talents.append(talent)
             else:
@@ -120,7 +109,11 @@ class AvatarListPlugin(Plugin, BasePlugin):
                     ],
                 )
             )
+            if len(avatar_datas) == max_length:
+                break
+        return avatar_datas
 
+    async def get_final_data(self, client: Client, characters: Sequence[Character], update: Update):
         try:
             response = await self.enka_client.fetch_user(client.uid)
             namecard = (await self.assets_service.namecard(response.player.namecard.id).navbar()).as_uri()
@@ -129,7 +122,7 @@ class AvatarListPlugin(Plugin, BasePlugin):
         except Exception as e:  # pylint: disable=W0703
             logger.debug(f"enka 请求失败: {e}")
             choices = ArkoWrapper(characters).filter(lambda x: x.friendship == 10)
-            if not [choices]:
+            if not choices:
                 choices = ArkoWrapper(characters).sort(lambda x: x.friendship, reverse=True)
             namecard_choices = (
                 ArkoWrapper(choices)
@@ -140,6 +133,36 @@ class AvatarListPlugin(Plugin, BasePlugin):
             namecard = (await self.assets_service.namecard(namecard_choices[0]).navbar()).as_uri()
             avatar = (await self.assets_service.avatar(choices[0].id).icon()).as_uri()
             nickname = update.effective_user.full_name
+        return namecard, avatar, nickname
+
+    @handler.command("avatars")
+    @handler.message(filters.Regex(r"^练度统计$"))
+    @restricts(30)
+    @error_callable
+    async def avatar_list(self, update: Update, context: CallbackContext):
+        user = update.effective_user
+        message = update.effective_message
+
+        logger.info(f"用户 {user.full_name}[{user.id}] [bold]练度统计[/bold]", extra={"markup": True})
+
+        client = await self.get_user_client(user, message, context)
+        if not client:
+            return
+
+        notice = await message.reply_text("派蒙需要收集整理数据，还请耐心等待哦~")
+        await message.reply_chat_action(ChatAction.TYPING)
+
+        characters = await client.get_genshin_characters(client.uid)
+
+        document, has_more = False, False
+        if filters.ChatType.GROUPS.filter(message):
+            document, has_more = False, True
+        else:
+            document = True
+
+        avatar_datas: List[AvatarData] = await self.get_avatars_data(characters, client, 20 if has_more else None)
+
+        namecard, avatar, nickname = await self.get_final_data(client, characters, update)
 
         render_data = {
             "uid": client.uid,
@@ -147,17 +170,23 @@ class AvatarListPlugin(Plugin, BasePlugin):
             "avatar": avatar,
             "namecard": namecard,
             "avatar_datas": avatar_datas,
+            "has_more": has_more,
         }
 
-        await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+        await message.reply_chat_action(ChatAction.UPLOAD_DOCUMENT if document else ChatAction.UPLOAD_PHOTO)
 
         image = await self.template_service.render(
-            "genshin/avatar_list/main.html", render_data, viewport={"width": 1040, "height": 500}, full_page=True
+            "genshin/avatar_list/main.html",
+            render_data,
+            viewport={"width": 1040, "height": 500},
+            full_page=True,
+            query_selector=".container",
         )
-
-        await message.reply_document(InputFile(image, filename=f"{user.full_name}的练度统计.png"))
-        if filters.ChatType.GROUPS.filter(message):
-            self._add_delete_message_job(context, notice.chat_id, notice.message_id, 10)
+        self._add_delete_message_job(context, notice.chat_id, notice.message_id, 5)
+        if document:
+            await message.reply_document(InputFile(image, filename="练度统计.png"))
+        else:
+            await message.reply_photo(image)
 
 
 class SkillData(Model):
