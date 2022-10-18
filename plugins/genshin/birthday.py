298ba3dec976dea5fa025f0ac81ea166bb29b98b
xtaodada <xtao@xtaolink.cn>
2022-10-18 15:11:47 +0800

    优化导入

--- plugins/genshin/birthday.py
+++ plugins/genshin/birthday.py
@@ -1,6 +1,4 @@
-import json
 from datetime import datetime
-from os import sep
 
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
@@ -8,6 +6,7 @@ from telegram.ext import filters
 
 from core.baseplugin import BasePlugin
 from core.plugin import Plugin, handler
+from metadata.genshin import AVATAR_DATA
 from utils.decorators.error import error_callable
 from utils.decorators.restricts import restricts
 from utils.log import logger
@@ -18,8 +17,12 @@ class BirthdayPlugin(Plugin, BasePlugin):
 
     def __init__(self):
         """加载数据文件."""
-        with open(f"resources{sep}json{sep}birthday.json", "r", encoding="utf8") as f:
-            self.birthday_list = json.load(f)
+        self.birthday_list = {}
+        for value in AVATAR_DATA.values():
+            key = "_".join([str(i) for i in value["birthday"]])
+            data = self.birthday_list.get(key, [])
+            data.append(value["name"])
+            self.birthday_list.update({key: data})
 
     @handler(CommandHandler, command="birthday", block=False)
     @restricts()
@@ -27,24 +30,13 @@ class BirthdayPlugin(Plugin, BasePlugin):
     async def command_start(self, update: Update, context: CallbackContext) -> None:
         message = update.effective_message
         user = update.effective_user
-        month = datetime.today().strftime("%m")
-        day = datetime.today().strftime("%d")
+        key = datetime.now().strftime("%m_%d")
         logger.info(f"用户 {user.full_name}[{user.id}] 查询今日角色生日列表")
-        try:
-            today_list = self.birthday_list[month][day]
-            len_today_list = len(today_list)
-            name = ""
-            for i in range(len_today_list):
-                name = f"{name},{today_list[i]}"
-            name = name.replace(",", "", 1)
-            reply_message = await update.effective_message.reply_text(f"今天是{name}的生日哦！")
-            if filters.ChatType.GROUPS.filter(reply_message):
-                self._add_delete_message_job(context, message.chat_id, message.message_id)
-                self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
-            return
-        except KeyError:
-            reply_message = await update.effective_message.reply_text("今天没有人过生日哦！")
-            if filters.ChatType.GROUPS.filter(reply_message):
-                self._add_delete_message_job(context, message.chat_id, message.message_id)
-                self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
-            return
+
+        today_list = self.birthday_list.get(key, [])
+        breakpoint()
+        text = f"今天是 {'、'.join(today_list)} 的生日哦~" if today_list else "今天没有角色过生日哦~"
+        reply_message = await update.effective_message.reply_text(text)
+        if filters.ChatType.GROUPS.filter(reply_message):
+            self._add_delete_message_job(context, message.chat_id, message.message_id)
+            self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
