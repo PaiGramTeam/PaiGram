fea43349b3ff9696e914d68c00e0f6699aa05f7f
zhxy-CN <admin@owo.cab>
2022-10-29 22:15:11 +0800

    ✨Query any character's birthday

--- plugins/genshin/birthday.py
+++ plugins/genshin/birthday.py
@@ -8,6 +8,7 @@ from telegram.ext import filters
 from core.baseplugin import BasePlugin
 from core.plugin import Plugin, handler
 from metadata.genshin import AVATAR_DATA
+from metadata.shortname import roleToId, roleToName
 from utils.bot import get_all_args
 from utils.decorators.error import error_callable
 from utils.decorators.restricts import restricts
@@ -15,10 +16,10 @@ from utils.log import logger
 
 
 class BirthdayPlugin(Plugin, BasePlugin):
-    """生日."""
+    """Birthday."""
 
     def __init__(self):
-        """加载数据文件."""
+        """Load Data."""
         self.birthday_list = {}
         for value in AVATAR_DATA.values():
             key = "_".join([str(i) for i in value["birthday"]])
@@ -46,13 +47,23 @@ class BirthdayPlugin(Plugin, BasePlugin):
                 if filters.ChatType.GROUPS.filter(reply_message):
                     self._add_delete_message_job(context, message.chat_id, message.message_id)
                     self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
-                return
             else:
-                reply_message = await update.effective_message.reply_text("请输入正确的日期格式，如 1-1。")
-                if filters.ChatType.GROUPS.filter(reply_message):
-                    self._add_delete_message_job(context, message.chat_id, message.message_id)
-                    self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
-                return
+                try:
+                    name = roleToName(msg)
+                    aid = str(roleToId(msg))
+                    birthday = AVATAR_DATA[aid]["birthday"]
+                    text = f"{name} 的生日是 {birthday[0]}月{birthday[1]}日 哦~"
+                    reply_message = await update.effective_message.reply_text(text)
+                    if filters.ChatType.GROUPS.filter(reply_message):
+                        self._add_delete_message_job(context, message.chat_id, message.message_id)
+                        self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
+                except KeyError:
+                    reply_message = await update.effective_message.reply_text(
+                        "请输入正确的日期格式，如1-1，或输入正确的角色名称。"
+                    )
+                    if filters.ChatType.GROUPS.filter(reply_message):
+                        self._add_delete_message_job(context, message.chat_id, message.message_id)
+                        self._add_delete_message_job(context, reply_message.chat_id, reply_message.message_id)
         else:
             logger.info(f"用户 {user.full_name}[{user.id}] 查询今日角色生日列表")
             today_list = self.birthday_list.get(key, [])
