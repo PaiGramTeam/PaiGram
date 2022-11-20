d096cd29a5dd0656936d501b5bf43b59d5583c62
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-11-20 13:59:16 +0800

    🐛 Fix material cache error

--- core/game/cache.py
+++ core/game/cache.py
@@ -11,12 +11,11 @@ class GameCache:
 
     async def get_url_list(self, character_name: str):
         qname = f"{self.qname}:{character_name}"
-        return [str(str_data, encoding="utf-8") for str_data in await self.client.lrange(qname, 0, -1)]
+        return [str(str_data, encoding="utf-8") for str_data in await self.client.lrange(qname, 0, -1)][::-1]
 
     async def set_url_list(self, character_name: str, str_list: List[str]):
         qname = f"{self.qname}:{character_name}"
         await self.client.ltrim(qname, 1, 0)
         await self.client.lpush(qname, *str_list)
         await self.client.expire(qname, self.ttl)
-        count = await self.client.llen(qname)
-        return count
+        return await self.client.llen(qname)
