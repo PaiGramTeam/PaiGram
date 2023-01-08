c48ec6243f7ad15beb3ace28cd4740208e9e0bf9
洛水居室 <luoshuijs@outlook.com>
2023-01-08 17:07:50 +0800

    🎨 Add `players` Service

---
+++ core/services/players/__init__.py
@@ -0,0 +1,3 @@
+from .services import PlayersService
+
+__all__ = ("PlayersService",)
---
+++ core/services/players/repositories.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+from sqlalchemy import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
+from utils.models.base import RegionEnum
+from .models import PlayersDataBase as Player
+
+__all__ = ("PlayersRepository",)
+
+from ...dependence.mysql import MySQL
+
+
+class PlayersRepository:
+    def __init__(self, mysql: MySQL):
+        self.engine = mysql.engine
+
+    async def get_by_user_id(self, user_id: int, region: Optional[RegionEnum]) -> Optional[Player]:
+        async with AsyncSession(self.engine) as session:
+            if region:
+                statement = select(Player).where(
+                    Player.user_id == user_id and Player.region == region and Player.is_chosen == 1
+                )
+            else:
+                statement = select(Player).where(Player.user_id == user_id and Player.is_chosen == 1)
+            results = await session.exec(statement)
+            return results.first()
+
+    async def add(self, player: Player):
+        async with AsyncSession(self.engine) as session:
+            session.add(player)
+            await session.commit()
+
+    async def remove(self, player: Player):
+        async with AsyncSession(self.engine) as session:
+            await session.delete(player)
+            await session.commit()
+
+    async def get_all_by_user_id(self, user_id: int) -> List[Player]:
+        async with AsyncSession(self.engine) as session:
+            statement = select(Player).where(Player.user_id == user_id)
+            results = await session.exec(statement)
+            players = results.all()
+            return players
