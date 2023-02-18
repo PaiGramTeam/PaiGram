c26016561a37af6f0480234003c97d883ee6ecdd
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2023-02-18 15:41:10 +0800

    ✨ Post support gif

--- modules/apihelper/client/components/hyperion.py
+++ modules/apihelper/client/components/hyperion.py
@@ -118,8 +118,11 @@ class Hyperion:
         return art_list
 
     async def download_image(self, art_id: int, url: str, page: int = 0) -> ArtworkImage:
-        response = await self.client.get(url, params=self.get_images_params(resize=2000), timeout=10, de_json=False)
-        return ArtworkImage(art_id=art_id, page=page, data=response.content)
+        image = url.endswith(".jpg") or url.endswith(".png")
+        response = await self.client.get(
+            url, params=self.get_images_params(resize=2000) if image else None, timeout=10, de_json=False
+        )
+        return ArtworkImage(art_id=art_id, page=page, ext=url.split(".")[-1], data=response.content)
 
     async def get_new_list(self, gids: int, type_id: int, page_size: int = 20):
         """
