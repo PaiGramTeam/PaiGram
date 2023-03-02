c87730f6c2b9cd17dc32d4824a3dc8cdc76ff061
洛水居室 <luoshuijs@outlook.com>
2023-03-02 12:07:21 +0800

    ✅ Update test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,6 +5,7 @@ import pytest_asyncio
 
 from core.config import config
 from core.dependence.mysql import MySQL
+from core.dependence.redisdb import RedisDB
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -18,5 +19,9 @@ def event_loop():
 
 @pytest.fixture(scope="session")
 def mysql():
-    _mysql = MySQL.from_config(config=config)
-    return _mysql
+    return MySQL.from_config(config=config)
+
+
+@pytest.fixture(scope="session")
+def redis():
+    return RedisDB.from_config(config=config)
