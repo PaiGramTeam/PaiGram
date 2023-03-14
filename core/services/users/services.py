21db8bb85db289c8c5394b9a58b0c9f701284e27
洛水居室 <luoshuijs@outlook.com>
2023-03-14 12:15:19 +0800

    🐛 Fix incorrect addition of admin list to cache

--- core/services/users/services.py
+++ core/services/users/services.py
@@ -49,6 +49,9 @@ class UserAdminService(BaseService):
                 await self.user_repository.add(user)
         else:
             logger.warning("检测到未配置Bot所有者 会导无法正常使用管理员权限")
+        users = await self.user_repository.get_all()
+        for user in users:
+            await self._cache.set(user.user_id)
 
     async def is_admin(self, user_id: int) -> bool:
         return await self._cache.ismember(user_id)
