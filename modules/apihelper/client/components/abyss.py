8ecc9f97a78f8cc8efeca65c0bb80d773645ca79
洛水居室 <luoshuijs@outlook.com>
2023-10-20 12:23:02 +0800

    :art: Update AbyssTeam Request Headers

--- modules/apihelper/client/components/abyss.py
+++ modules/apihelper/client/components/abyss.py
@@ -10,9 +10,8 @@ class AbyssTeam:
     TEAM_RATE_API = "https://homa.snapgenshin.com/Statistics/Team/Combination"
     HEADERS = {
         "Host": "homa.snapgenshin.com",
-        "Referer": "https://servicewechat.com/wxce4dbe0cb0f764b3/91/page-frame.html",
-        "User-Agent": "Mozilla/5.0 (iPad; CPU OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
-        "Mobile/15E148 MicroMessenger/8.0.20(0x1800142f) NetType/WIFI Language/zh_CN",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/118.0.0.0 Safari/537.36 PaiGram/4.0",
         "content-type": "application/json",
     }
 
