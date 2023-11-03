a5ffb257c29274d6cc5f7e51cf6b9c5304d6dbc3
xtaodada <xtao@xtaolink.cn>
2023-11-03 23:41:47 +0800

    :bug: Fix the exception raised when both sa_column and primary_key are passed simultaneously

--- core/services/quiz/models.py
+++ core/services/quiz/models.py
@@ -10,9 +10,7 @@ class AnswerDB(SQLModel, table=True):
     __tablename__ = "answer"
     __table_args__ = dict(mysql_charset="utf8mb4", mysql_collate="utf8mb4_general_ci")
 
-    id: Optional[int] = Field(
-        default=None, primary_key=True, sa_column=Column(Integer, primary_key=True, autoincrement=True)
-    )
+    id: Optional[int] = Field(default=None, sa_column=Column(Integer, primary_key=True, autoincrement=True))
     question_id: Optional[int] = Field(
         sa_column=Column(Integer, ForeignKey("question.id", ondelete="RESTRICT", onupdate="RESTRICT"))
     )
@@ -24,9 +22,7 @@ class QuestionDB(SQLModel, table=True):
     __tablename__ = "question"
     __table_args__ = dict(mysql_charset="utf8mb4", mysql_collate="utf8mb4_general_ci")
 
-    id: Optional[int] = Field(
-        default=None, primary_key=True, sa_column=Column(Integer, primary_key=True, autoincrement=True)
-    )
+    id: Optional[int] = Field(default=None, sa_column=Column(Integer, primary_key=True, autoincrement=True))
     text: Optional[str] = Field()
 
 
