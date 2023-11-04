d3f1425d09e7acef7750cbc54cf4c92821dc8529
kotoriのねこ <minamiktr@outlook.com>
2023-11-04 01:42:15 -0400

    :sparkles: Define DATA_DIR in path constants
    Co-authored-by: omg-xtao <100690902+omg-xtao@users.noreply.github.com>

--- utils/const/_path.py
+++ utils/const/_path.py
@@ -1,7 +1,7 @@
 """目录常量"""
 from pathlib import Path
 
-__all__ = ["PROJECT_ROOT", "CORE_DIR", "PLUGIN_DIR", "RESOURCE_DIR", "CACHE_DIR"]
+__all__ = ["PROJECT_ROOT", "CORE_DIR", "PLUGIN_DIR", "RESOURCE_DIR", "CACHE_DIR", "METADATA_DIR", "DATA_DIR"]
 
 # 项目根目录
 PROJECT_ROOT = Path(__file__).joinpath("../../..").resolve()
@@ -13,6 +13,10 @@ PLUGIN_DIR = PROJECT_ROOT / "plugins"
 RESOURCE_DIR = PROJECT_ROOT / "resources"
 # cache 目录
 CACHE_DIR = PROJECT_ROOT / "cache"
+# metadata 目录
+METADATA_DIR = PROJECT_ROOT / "metadata" / "data"
+# data 目录
+DATA_DIR = PROJECT_ROOT / "data"
 
 if not CACHE_DIR.exists():
     CACHE_DIR.mkdir(exist_ok=True, parents=True)
