33ffbab4ab586659b93d7103b4103df49d59a8f8
xtaodada <xtao@xtaolink.cn>
2024-01-16 13:56:53 +0800

    :sparkles: Add group manage service

---
+++ core/services/groups/cache.py
@@ -0,0 +1,3 @@
+from gram_core.services.groups.cache import GroupBanCache, GroupUpdateCache
+
+__all__ = ("GroupBanCache", "GroupUpdateCache", )
---
+++ core/services/groups/repositories.py
@@ -0,0 +1,3 @@
+from gram_core.services.groups.repositories import GroupRepository
+
+__all__ = ("GroupRepository",)
