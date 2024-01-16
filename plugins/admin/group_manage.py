0ef15ae65c3c236f2d3a90775abf3ee55c6a14e7
xtaodada <xtao@xtaolink.cn>
2024-01-16 21:55:26 +0800

    add add_block and del_block commands

--- plugins/admin/group_manage.py
+++ plugins/admin/group_manage.py
@@ -1,11 +1,14 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from telegram.constants import ChatType
+from telegram.error import BadRequest, Forbidden
 from telegram.ext import ChatMemberHandler
 
 from core.handler.grouphandler import GroupHandler
 from core.plugin import Plugin, handler
 from core.services.groups import GroupService
+from core.services.groups.models import GroupDataBase as Group
+from core.services.users import UserBanService
 from utils.chatmember import extract_status_change
 from utils.log import logger
 
@@ -18,9 +21,11 @@ class GroupManage(Plugin):
     def __init__(
         self,
         group_service: GroupService,
+        user_ban_service: UserBanService,
     ):
         self.type_handler = None
         self.group_service = group_service
+        self.user_ban_service = user_ban_service
 
     async def initialize(self) -> None:
         self.type_handler = GroupHandler(self.application)
@@ -49,3 +54,88 @@ class GroupManage(Plugin):
                 await self.group_service.join(chat.id)
         if was_member and not is_member:
             await self.group_service.leave(chat.id)
+
+    def get_chat_id(self, context: "ContextTypes.DEFAULT_TYPE") -> Optional[int]:
+        args = self.get_args(context)
+        if not args:
+            return
+        try:
+            return int(args[0])
+        except ValueError:
+            return
+
+    async def add_block_group(self, chat_id: int):
+        group = await self.group_service.get_group_by_id(chat_id)
+        if group:
+            group.is_banned = True
+            await self.group_service.update_group(group)
+        else:
+            chat = None
+            try:
+                chat = await self.get_chat(chat_id)
+            except (BadRequest, Forbidden) as exc:
+                logger.warning(f"通过 id 获取会话信息失败，API 返回：{exc.message}")
+            if chat:
+                group = Group.from_chat(chat)
+            else:
+                group = Group.from_id(chat_id)
+            group.is_banned = True
+            await self.group_service.update_group(group)
+
+    @handler.command(command="add_block", block=False, admin=True)
+    async def add_block(self, update: "Update", context: "ContextTypes.DEFAULT_TYPE"):
+        user = update.effective_user
+        message = update.effective_message
+        chat_id = self.get_chat_id(context)
+        logger.info("用户 %s[%s] add_block 命令请求 chat_id[%s]", user.full_name, user.id, chat_id)
+        if not chat_id:
+            await message.reply_text("参数错误，请指定群 id ！")
+            return
+        if chat_id < 0:
+            if await self.group_service.is_banned(chat_id):
+                await message.reply_text("该群已在黑名单中！")
+                return
+            await self.add_block_group(chat_id)
+            await message.reply_text("已将该群加入黑名单！")
+        else:
+            if await self.user_ban_service.is_banned(chat_id):
+                await message.reply_text("该用户已在黑名单中！")
+                return
+            try:
+                await self.user_ban_service.add_ban(chat_id)
+            except PermissionError:
+                await message.reply_text("无法操作管理员！")
+                return
+            await message.reply_text("已将该用户加入黑名单！")
+
+    @handler.command(command="del_block", block=False, admin=True)
+    async def del_block(self, update: "Update", context: "ContextTypes.DEFAULT_TYPE"):
+        user = update.effective_user
+        message = update.effective_message
+        chat_id = self.get_chat_id(context)
+        logger.info("用户 %s[%s] del_block 命令请求 chat_id[%s]", user.full_name, user.id, chat_id)
+        if not chat_id:
+            await message.reply_text("参数错误，请指定群 id ！")
+            return
+        if chat_id < 0:
+            if not await self.group_service.is_banned(chat_id):
+                await message.reply_text("该群不在黑名单中！")
+                return
+            success = await self.group_service.del_ban(chat_id)
+            if not success:
+                await message.reply_text("该群不在黑名单中！")
+                return
+            await message.reply_text("已将该群移出黑名单！")
+        else:
+            if not await self.user_ban_service.is_banned(chat_id):
+                await message.reply_text("该用户不在黑名单中！")
+                return
+            try:
+                success = await self.user_ban_service.del_ban(chat_id)
+                if not success:
+                    await message.reply_text("该用户不在黑名单中！")
+                    return
+            except PermissionError:
+                await message.reply_text("无法操作管理员！")
+                return
+            await message.reply_text("已将该用户移出黑名单！")
