1a2aabc82339e32157fc36f525c78b123be4f43b
xtaodada <xtao@xtaolink.cn>
2024-03-02 21:37:03 +0800

    support  auto  update  task

--- plugins/admin/post_code.py
+++ plugins/admin/post_code.py
@@ -1,6 +1,6 @@
 import json
 import re
-from datetime import timezone, timedelta
+from datetime import datetime, timezone, timedelta
 from typing import List, Optional, Tuple, TYPE_CHECKING, Dict
 
 from httpx import Timeout
@@ -9,7 +9,7 @@ from telegram import (
     ReplyKeyboardRemove,
 )
 from telegram.constants import ParseMode
-from telegram.error import BadRequest
+from telegram.error import BadRequest, Forbidden
 from telegram.ext import ConversationHandler, filters
 
 from core.config import config
@@ -19,8 +19,8 @@ from modules.apihelper.error import APIHelperException
 from utils.log import logger
 
 if TYPE_CHECKING:
-    from telegram import Update
-    from telegram.ext import ContextTypes
+    from telegram import Update, Message
+    from telegram.ext import ContextTypes, Job
     from modules.apihelper.models.genshin.hyperion import LiveCode, LiveCodeHoYo
 
 
@@ -33,22 +33,59 @@ class PostCodeHandlerData:
         self.hoyo_code: List["LiveCodeHoYo"] = []
         self.channel_username: str = ""
         self.channel_id: int = -1
+        self.channel_msg: Optional["Message"] = None
+        self.need_update: bool = False
+        self.end_task_time: datetime = self.utc_8(datetime.now() + timedelta(hours=1))
+
+    def need_end_task(self) -> bool:
+        if not self.real_need_update():
+            return True
+        now = self.utc_8(datetime.now())
+        out_of_time = now >= self.end_task_time
+        if out_of_time:
+            logger.warning("PostCode 定时任务超过最大允许时间，结束任务")
+        return out_of_time
+
+    @staticmethod
+    def utc_8(time: datetime) -> datetime:
+        return time.astimezone(timezone(timedelta(hours=8)))
 
     def get_end_time(self) -> str:
         for code in self.hoyo_code:
             if code.offline_at:
-                utc_8 = code.offline_at.astimezone(timezone(timedelta(hours=8)))
+                utc_8 = self.utc_8(code.offline_at)
                 return utc_8.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_guess_last_time(self) -> datetime:
+        return self.utc_8(self.mys_code[-1].to_get_time)
+
+    def get_need_update_text(self):
+        time = []
+        for code in self.mys_code:
+            time.append(self.utc_8(code.to_get_time).strftime("%H:%M:%S"))
+        return UPDATE_TEMPLATE % tuple(time)
+
+    def real_need_update(self) -> bool:
+        return not all([i.code for i in self.mys_code] + [i.exchange_code for i in self.hoyo_code])
+
+    def get_code_text(self) -> List[str]:
+        return [code.text for code in self.mys_code] + [code.text for code in self.hoyo_code]
+
+    def have_changes(self, mys_code: List["LiveCode"], hoyo_code: List["LiveCodeHoYo"]) -> bool:
+        if len(mys_code) != len(self.mys_code) or len(hoyo_code) != len(self.hoyo_code):
+            return True
+        for i in range(len(mys_code)):
+            if mys_code[i].code != self.mys_code[i].code:
+                return True
+        for i in range(len(hoyo_code)):
+            if hoyo_code[i].exchange_code != self.hoyo_code[i].exchange_code:
+                return True
+        return False
 
     def get_text(self) -> str:
         return POST_TEMPLATE % (
             self.version,
-            self.mys_code[0].text,
-            self.mys_code[1].text,
-            self.mys_code[2].text,
-            self.hoyo_code[0].text,
-            self.hoyo_code[1].text,
-            self.hoyo_code[2].text,
+            *self.get_code_text(),
             self.get_end_time(),
         )
 
@@ -67,12 +104,17 @@ POST_TEMPLATE = """<b>《原神》%s 版本前瞻特别节目兑换码</b>
 <code>%s</code> - 原石 ×100，摩拉 x50000
 
 兑换码过期时间 %s UTC+8，请尽快领取。"""
+UPDATE_TEMPLATE = """可能的兑换码发放时间：
+
+%s、%s、%s
+
+更新可能延迟三到五分钟，请耐心等待。"""
 
 
 class PostCode(Plugin.Conversation):
     """版本前瞻特别节目兑换码推送"""
 
-    MENU_KEYBOARD = ReplyKeyboardMarkup([["推送频道", "退出"]], True, True)
+    MENU_KEYBOARD = ReplyKeyboardMarkup([["推送频道", "推送并且定时更新"], ["退出"]], True, True)
     SUBJECT_RE = re.compile(r"一起来看《原神》(\d+\.\d+)版本前瞻特别节目吧！")
     ACT_RE = re.compile(r"act_id=(.*?)&")
 
@@ -88,7 +130,7 @@ class PostCode(Plugin.Conversation):
                 read=config.read_timeout,
                 write=config.write_timeout,
                 pool=config.pool_timeout,
-            )
+            ),
         )
 
     def init_version(self, news: List[Dict]) -> Tuple[Optional[str], Optional[Dict]]:
@@ -148,10 +190,8 @@ class PostCode(Plugin.Conversation):
         user = update.effective_user
         message = update.effective_message
         logger.info("用户 %s[%s] POST_CODE 命令请求", user.full_name, user.id)
-        post_code_handler_data = context.chat_data.get("post_code_handler_data")
-        if post_code_handler_data is None:
-            post_code_handler_data = PostCodeHandlerData()
-            context.chat_data["post_code_handler_data"] = post_code_handler_data
+        post_code_handler_data = PostCodeHandlerData()
+        context.chat_data["post_code_handler_data"] = post_code_handler_data
         text = f"✿✿ヽ（°▽°）ノ✿ 你好！ {user.username} ，正在尝试自动获取必要的信息，请耐心等待。。。"
         reply = await message.reply_text(text)
         try:
@@ -170,11 +210,19 @@ class PostCode(Plugin.Conversation):
     @conversation.state(state=CHECK_COMMAND)
     @handler.message(filters=filters.TEXT & ~filters.COMMAND, block=False)
     async def check_command(self, update: "Update", context: "ContextTypes.DEFAULT_TYPE") -> int:
+        post_code_handler_data: PostCodeHandlerData = context.chat_data.get("post_code_handler_data")
         message = update.effective_message
         if message.text == "退出":
             await message.reply_text("退出任务", reply_markup=ReplyKeyboardRemove())
             return ConversationHandler.END
         if message.text == "推送频道":
+            return await self.get_channel(update, context)
+        if message.text == "推送并且定时更新":
+            if not post_code_handler_data.real_need_update():
+                await message.reply_text("所有兑换码已发放，无需创建更新任务，将直接推送。", reply_markup=ReplyKeyboardRemove())
+                return await self.get_channel(update, context)
+            post_code_handler_data.need_update = True
+            await message.reply_text(post_code_handler_data.get_need_update_text())
             return await self.get_channel(update, context)
         return ConversationHandler.END
 
@@ -230,10 +278,57 @@ class PostCode(Plugin.Conversation):
         post_text = post_code_handler_data.get_text()
         post_text += f"\n\n@{channel_username}"
         try:
-            await context.bot.send_message(channel_id, post_text, parse_mode=ParseMode.HTML)
+            msg = await context.bot.send_message(channel_id, post_text, parse_mode=ParseMode.HTML)
+            if post_code_handler_data.need_update:
+                post_code_handler_data.channel_msg = msg
+                end_time = post_code_handler_data.get_guess_last_time() + timedelta(minutes=10)
+                post_code_handler_data.end_task_time = end_time
+                self.create_task(post_code_handler_data)
         except BadRequest as exc:
             await message.reply_text(f"发送消息时发生错误 {exc.message}", reply_markup=ReplyKeyboardRemove())
             logger.error("PostCode 模块发送消息时发生错误 %s", exc.message)
             return ConversationHandler.END
         await message.reply_text("推送成功", reply_markup=ReplyKeyboardRemove())
         return ConversationHandler.END
+
+    def create_task(self, data: "PostCodeHandlerData"):
+        logger.debug("创建 PostCode 定时任务")
+        self.application.job_queue.run_once(self.post_code_task, 60, data=data)
+
+    async def post_code_task(self, context: "ContextTypes.DEFAULT_TYPE"):
+        client = self.get_bbs_client()
+        job: "Job" = context.job
+        if not isinstance(job.data, PostCodeHandlerData):
+            return
+        post_code_handler_data: "PostCodeHandlerData" = job.data
+        if not post_code_handler_data.channel_msg:
+            return
+        act_id = post_code_handler_data.act_id
+        ver_code = post_code_handler_data.ver_code
+        channel_username = post_code_handler_data.channel_username
+        try:
+            mys_code = await client.get_live_code(act_id, ver_code)
+            hoyo_code = await client.get_live_code_hoyo(self.gids)
+            if len(post_code_handler_data.mys_code) != 3 or len(post_code_handler_data.hoyo_code) != 3:
+                raise ValueError("获取兑换码数据成功，但是数量不对")
+            if post_code_handler_data.have_changes(mys_code, hoyo_code):
+                post_code_handler_data.mys_code = mys_code
+                post_code_handler_data.hoyo_code = hoyo_code
+                post_text = post_code_handler_data.get_text()
+                post_text += f"\n\n@{channel_username}"
+                await post_code_handler_data.channel_msg.edit_text(post_text, parse_mode=ParseMode.HTML)
+                logger.success("PostCode 兑换码发生变化，已更新频道消息")
+            else:
+                logger.debug("PostCode 兑换码未发生变化")
+        except (APIHelperException, ValueError) as exc:
+            logger.error("获取兑换码时发生错误 %s", str(exc))
+        except BadRequest as exc:
+            logger.error("自动更新兑换码消息失败 Message[%s]", exc.message)
+        except Forbidden as exc:
+            logger.error("自动更新兑换码消息失败 message[%s]", exc.message)
+        except Exception as exc:
+            logger.error("自动更新兑换码消息失败", exc_info=exc)
+        if post_code_handler_data.need_end_task():
+            logger.success("PostCode 定时任务结束")
+        else:
+            self.create_task(post_code_handler_data)
