628f43ec48c6ec2652cb02bb20f562f5f49dee80
xtaodada <xtao@xtaolink.cn>
2024-09-27 21:56:27 +0800

    :alien: Use new ambr api host

--- utils/const/_url.py
+++ utils/const/_url.py
@@ -4,6 +4,6 @@ __all__ = ("HONEY_HOST", "ENKA_HOST", "AMBR_HOST", "AMBR_API_HOST", "CELESTIA_HO
 
 HONEY_HOST = URL("https://gensh.honeyhunterworld.com/")
 ENKA_HOST = URL("https://enka.network/")
-AMBR_HOST = URL("https://gi.yatta.top/")
+AMBR_HOST = URL("https://gi.yatta.moe/")
 AMBR_API_HOST = AMBR_HOST.join("api/")
 CELESTIA_HOST = URL("https://www.projectcelestia.com/")
