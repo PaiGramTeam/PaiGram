4c4d69a4928e4cedd160a9b854ff0655e6d09eba
xtaodada <xtao@xtaolink.cn>
2024-12-02 16:46:49 +0800

    :bug: Fix pydantic v2 basemodel config ignore extra

--- utils/patch/pydantic_basemodel.py
+++ utils/patch/pydantic_basemodel.py
@@ -1,5 +1,4 @@
 import pydantic
 
-pydantic.BaseModel.model_config["extra"] = "allow"
 pydantic.BaseModel.model_config["coerce_numbers_to_str"] = True
 pydantic.BaseModel.model_config["arbitrary_types_allowed"] = True
