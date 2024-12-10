e3ff98f1ad59e752de220d5f7045234285b6d037
xtaodada <xtao@xtaolink.cn>
2024-12-10 14:21:45 +0800

    :adhesive_bandage: Change Cloud Game Sign Job Time to 05:01

--- plugins/jobs/cloud_game_sign.py
+++ plugins/jobs/cloud_game_sign.py
@@ -14,7 +14,7 @@ class SignJob(Plugin):
     def __init__(self, sign_system: CloudGameHelper):
         self.sign_system = sign_system
 
-    @job.run_daily(time=datetime.time(hour=0, minute=1, second=0), name="CloudGameSignJob")
+    @job.run_daily(time=datetime.time(hour=5, minute=1, second=0), name="CloudGameSignJob")
     async def sign(self, context: "ContextTypes.DEFAULT_TYPE"):
         logger.info("正在执行云游戏自动签到")
         await self.sign_system.do_sign_job(context, job_type=SignJobType.START)
